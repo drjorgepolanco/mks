e15ac75fa5a318aac86ac870c80572d915db5e7e
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-06 19:32:28 -0500

    Uncomment code

--- immersive_prework/ruby/class.rb
+++ immersive_prework/ruby/class.rb
@@ -1,28 +1,28 @@
 # Pet
 # ***
 
-# Pet = Class.new do
-# 	def speak(noise)
-# 		puts "The dog says #{noise}."
-# 	end
+Pet = Class.new do
+	def speak(noise)
+		puts "The dog says #{noise}."
+	end
 
-# 	def run(speed)
-# 		puts "The dog runs " + speed + "."
-# 	end
+	def run(speed)
+		puts "The dog runs " + speed + "."
+	end
 
-# 	def cry(level)
-# 		puts "The dog cries #{level}." 
-# 	end
-# end
+	def cry(level)
+		puts "The dog cries #{level}." 
+	end
+end
 
-# user_noise = puts "What kind of noise do you want the dog to make?"
-# user_noise = gets.chomp
+user_noise = puts "What kind of noise do you want the dog to make?"
+user_noise = gets.chomp
 
-# dog = Pet.new
+dog = Pet.new
 
-# dog.speak(user_noise)
-# dog.run("fast")
-# dog.cry("loud")
+dog.speak(user_noise)
+dog.run("fast")
+dog.cry("loud")
 
 
 
@@ -30,20 +30,20 @@
 # Person
 # ******
 
-# class Person
-# 	def data(name, age)
-# 		@name = name
-# 		@age = age
-# 	end
+class Person
+	def data(name, age)
+		@name = name
+		@age = age
+	end
 
-# 	def interest(action, lazy)
-# 		puts "#{@name}, who is #{@age} years old, likes #{action} when he is feeling active and #{lazy} when he is feeling quiet."
-# 	end
-# end
+	def interest(action, lazy)
+		puts "#{@name}, who is #{@age} years old, likes #{action} when he is feeling active and #{lazy} when he is feeling quiet."
+	end
+end
 
 
-# juan = Person.new
-# juan.data("Juan", 30).interest("parachuting", "reading")
+juan = Person.new
+juan.data("Juan", 30).interest("parachuting", "reading")
 
 
 
@@ -51,22 +51,22 @@
 # Marker
 # ******
 
-# class Marker
-# 	def set_color(my_color)
-# 		@color = my_color
-# 	end
+class Marker
+	def set_color(my_color)
+		@color = my_color
+	end
 
-# 	def write
-# 		puts "I am writing with a #{@color} marker!"
-# 	end
-# end
+	def write
+		puts "I am writing with a #{@color} marker!"
+	end
+end
 
-# red_marker = Marker.new
-# green_marker = Marker.new
-# red_marker.set_color("red")
-# green_marker.set_color("green")
-# red_marker.write
-# green_marker.write
+red_marker = Marker.new
+green_marker = Marker.new
+red_marker.set_color("red")
+green_marker.set_color("green")
+red_marker.write
+green_marker.write
 
 
 
