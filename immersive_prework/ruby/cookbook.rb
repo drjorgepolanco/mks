948d357e5e6e0c26cf13b7296d1976a1f01e5128
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-12 15:48:54 -0500

    Refactor code to use attribute reader, writer and accessor.

--- immersive_prework/ruby/cookbook.rb
+++ immersive_prework/ruby/cookbook.rb
@@ -1,46 +1,19 @@
 class Cookbook
 	attr_accessor :title
+
 	def initialize(title)
 		@title = title
-	end
-
-	def title
-		@title
-	end
-
-	def title=(new_title)
-		@title = new_title
 	end
 end
 
 class Recipe
+	attr_reader :title
+	attr_accessor :ingredients
+	attr_writer :steps
+
 	def initialize(title, ingredients, steps)
 		@title = title
 		@ingredients = ingredients
 		@steps = steps
-	end
-
-	def title
-		@title
-	end
-
-	def ingredients
-		@ingredients
-	end
-
-	def steps
-		@steps
-	end
-
-	def title=(new_title)
-		@title = new_title
-	end
-
-	def ingredients=(new_ingredients)
-		@ingredients = new_ingredients
-	end
-
-	def steps=(new_steps)
-		@steps = new_steps
 	end
 end
