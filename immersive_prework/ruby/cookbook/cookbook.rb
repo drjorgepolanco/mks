ad634f1ec6e85909554fabb4abb6daa0dae35643
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-12 16:25:24 -0500

    Put files in cookbook folder, add testcode

---
+++ immersive_prework/ruby/cookbook/cookbook.rb
@@ -0,0 +1,25 @@
+class Cookbook
+	attr_accessor :title
+	attr_reader :recipes
+
+	def initialize(title)
+		@title = title
+		@recipes = []
+	end
+
+	def add_recipe(recipe)
+		@recipes << recipe
+	end
+end
+
+class Recipe
+	attr_accessor :title
+	attr_accessor :ingredients
+	attr_accessor :steps
+
+	def initialize(title, ingredients, steps)
+		@title = title
+		@ingredients = ingredients
+		@steps = steps
+	end
+end
