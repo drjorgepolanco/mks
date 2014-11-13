362d3df7cb418678609e336cab74256153f13b7f
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-12 20:14:13 -0500

    Add print_recipe method to Recipes, comment code in testcode.rb, add separators to easily understand code

--- immersive_prework/ruby/cookbook/cookbook.rb
+++ immersive_prework/ruby/cookbook/cookbook.rb
@@ -13,6 +13,7 @@ class Cookbook
 	end
 
 	def recipe_titles
+		puts "================================================================="
 		puts "The available recipes in this Book are: "
 		@recipes.each do |item| 
 			puts item.title
@@ -20,24 +21,34 @@ class Cookbook
 	end
 
 	def recipe_ingredients
+		puts "================================================================="
 		puts "The ingredients of this recipe are: "
 		@recipes.each {|item| puts item.ingredients}
 	end
 
 	def recipe_steps
+		puts "================================================================="
 		puts "To make this recipe, you need to: "
 		@recipes.each {|item| puts item.steps}
 	end
 end
 
 class Recipe
-	attr_accessor :title
-	attr_accessor :ingredients
-	attr_accessor :steps
+	attr_accessor :title, :ingredients, :steps
 
 	def initialize(title, ingredients, steps)
 		@title = title
 		@ingredients = ingredients
 		@steps = steps
+	end
+
+	def print_recipe
+		puts "================================================================="
+		puts "Title:"
+		puts @title
+		puts "Ingredients:"
+		puts @ingredients
+		puts "Steps:"
+		puts @steps
 	end
 end
--- immersive_prework/ruby/cookbook/testcode.rb
+++ immersive_prework/ruby/cookbook/testcode.rb
@@ -2,27 +2,32 @@ require_relative 'cookbook'
 
 mex_cuisine = Cookbook.new("Mexican Cooking")
 burrito = Recipe.new("Bean Burrito", ["tortilla", "bean"], ["heat beans", "place beans in tortilla", "roll up"])
+burrito_veggie = Recipe.new("Veggie Burrito", ["tortilla", "veggies", "salt"], ["heat veggies", "place veggies in tortilla", "put salt", "roll up"])
 
 puts mex_cuisine.title
-puts burrito.title
-p burrito.ingredients 
-p burrito.steps 
+# puts burrito.title
+# p burrito.ingredients 
+# p burrito.steps 
 
 mex_cuisine.title = "Mexican Recipes"
 puts mex_cuisine.title 
 
-burrito.title = "Veggie Burrito"
-burrito.ingredients = ["tortilla", "tomatoes"]
-burrito.steps = ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
+# burrito.title = "Veggie Burrito"
+# burrito.ingredients = ["tortilla", "tomatoes"]
+# burrito.steps = ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
 
-p burrito.title 
-p burrito.ingredients 
+# p burrito.title 
+# p burrito.ingredients 
 
 
-mex_cuisine.recipes 
+# mex_cuisine.recipes 
 mex_cuisine.add_recipe(burrito)
-p mex_cuisine.recipes
+mex_cuisine.add_recipe(burrito_veggie)
+# puts mex_cuisine.recipes
 
-mex_cuisine.recipe_titles
-mex_cuisine.recipe_ingredients 
-mex_cuisine.recipe_steps
+# mex_cuisine.recipe_titles
+# mex_cuisine.recipe_ingredients 
+# mex_cuisine.recipe_steps
+
+burrito.print_recipe
+burrito_veggie.print_recipe
