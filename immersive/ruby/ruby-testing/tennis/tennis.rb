5ef777e9fe5e2b4c282194e3893949e0ca3bc776
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-14 11:15:18 -0500

    Delete comments

--- immersive/ruby/ruby-testing/tennis/tennis.rb
+++ immersive/ruby/ruby-testing/tennis/tennis.rb
@@ -10,14 +10,7 @@ module Tennis
       @player2.opponent = @player1
     end
 
-    # Records a win for a ball in a game.
-    #
-    # winner - The Integer (1 or 2) representing the winning player.
-    #
-    # Returns the score of the winning player. 
     def wins_ball(winner)
-      # TODO: Think it's gross to pass an integer instead of a player object?
-      # Then reimplement this method!
       winner.record_won_ball!
     end
   end
@@ -29,14 +22,10 @@ module Tennis
       @points = 0
     end
 
-    # Increments the score by 1.
-    #
-    # Returns the integer new score.
     def record_won_ball!
       @points += 1
     end
 
-    # Returns the String score for the player.
     def score
       return 'love' if @points == 0
       return 'fifteen' if @points == 1
