87d4caf67c141ba64fc4a03fa163635280570c9c
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-13 21:33:24 -0500

    Add test to check that the Pie has all it's toppings

--- immersive/ruby/ruby-testing/pizza/spec/pizza_spec.rb
+++ immersive/ruby/ruby-testing/pizza/spec/pizza_spec.rb
@@ -2,7 +2,15 @@ require './pizza'
 
 describe Pizza::Pie do
 	describe '.initialize' do
+		it "records all of the toppings" do
+			toppings = [
+				Pizza::Topping.new("mushrooms", vegetarian: true),
+				Pizza::Topping.new("pepperoni")
+			]
+			pizza = Pizza::Pie.new(toppings)
 
+			expect(pizza.toppings).to eq(toppings)
+		end
 	end
 end
 
