1a1b0510f2cc96a39a2a8aba3ed84f9581ebbfdb
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-14 08:27:48 -0500

    Add an extra case in the score method to handle the strikes.

--- immersive/ruby/ruby-testing/bowling/bowling.rb
+++ immersive/ruby/ruby-testing/bowling/bowling.rb
@@ -20,15 +20,25 @@ class BowlingGame
 			roll 			= @rolls[current_roll]
 			next_roll = @rolls[current_roll + 1]
 
-			if roll + next_roll == 10
+			if roll == 10
+				total_score += 10 + next_roll + @rolls[current_roll + 2]
+				current_roll += 1
+			elsif roll + next_roll == 10
 				total_score += 10 + @rolls[current_roll + 2]
+				current_roll += 2
 			else
 				total_score += roll + next_roll
+				current_roll += 2
 			end
-
-			current_roll += 2
 		end
 
 		return total_score
 	end
 end
+
+# Bowling is 10 frames long, and normally you get two rolls for each frame, 
+# or 20 total per game (unless you get strikes or spares).
+
+# Spare: the score for the frame is 10 plus the number of pins knocked down on the next roll.
+
+# Strike: score for the frame is ten plus the simple total of the pins knocked down in the next two rolls.
