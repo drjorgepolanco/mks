003773ab0d1ff88acc69e4ef95a6f66c33bdde8b
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-13 20:44:58 -0500

    Ruby Testing Part 1. Pizza App & Rspec. Initial commit.

---
+++ immersive/ruby/ruby-testing/pizza/pizza.rb
@@ -0,0 +1,5 @@
+class Pizza
+end
+
+class Topping
+end
---
+++ immersive/ruby/ruby-testing/pizza/spec/pizza_spec.rb
@@ -0,0 +1,13 @@
+require './pizza'
+
+describe Pizza do
+	it "exists" do
+		expect(Pizza).to be_a(Class)
+	end
+end
+
+describe Topping do
+	it "exists" do
+		expect(Topping).to be_a(Class)
+	end
+end
