a04cf34ca992dc65f49d38963413af34c06703a5
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-14 10:00:52 -0500

    Refactoring. Create new small methods (init_roll, strike? and score_strike) to clean method 'score'

--- immersive/ruby/ruby-testing/bowling/bowling.rb
+++ immersive/ruby/ruby-testing/bowling/bowling.rb
@@ -3,6 +3,8 @@ class BowlingGame
 
 	def initialize
 		@rolls = []
+		@total_score = 0
+		@current_roll = 0
 	end
 
 	def roll(pins)
@@ -10,30 +12,39 @@ class BowlingGame
 	end
 
 	def score
-		# reduce: sum the elements of an array
-		# @rolls.reduce(:+)
+		while @current_roll < @rolls.size
+			init_roll
 
-		total_score = 0
-		current_roll = 0
-
-		while current_roll < @rolls.size
-			roll 			= @rolls[current_roll]
-			next_roll = @rolls[current_roll + 1]
-
-			if roll == 10
-				total_score += 10 + next_roll + @rolls[current_roll + 2]
-				current_roll += 1
-			elsif roll + next_roll == 10
-				total_score += 10 + @rolls[current_roll + 2]
-				current_roll += 2
+			if strike?
+				score_strike
+			elsif @roll + @next_roll == 10
+				@total_score += 10 + @rolls[@current_roll + 2]
+				@current_roll += 2
 			else
-				total_score += roll + next_roll
-				current_roll += 2
+				@total_score += @roll + @next_roll
+				@current_roll += 2
 			end
 		end
 
-		return total_score
+		return @total_score
 	end
+
+	private
+
+	def init_roll
+		@roll 			= @rolls[@current_roll]
+		@next_roll 	= @rolls[@current_roll + 1]
+	end
+
+	def strike?
+		@roll == 10
+	end
+
+	def score_strike
+		@total_score += 10 + @next_roll + @rolls[@current_roll + 2]
+		@current_roll += 1
+	end
+
 end
 
 # Bowling is 10 frames long, and normally you get two rolls for each frame, 
