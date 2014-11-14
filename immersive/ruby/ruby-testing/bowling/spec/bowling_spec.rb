10b7d92d68650c2ee1b5eb04bb1891bbdbeafe73
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-14 07:54:15 -0500

    Add context scenarios to 'score' tests

--- immersive/ruby/ruby-testing/bowling/spec/bowling_spec.rb
+++ immersive/ruby/ruby-testing/bowling/spec/bowling_spec.rb
@@ -3,9 +3,9 @@ require './bowling'
 describe BowlingGame do
 	let(:game) {BowlingGame.new}
 
-	it "exists" do
-		expect(BowlingGame).to be_a(Class)
-	end
+	# it "exists" do
+	# 	expect(BowlingGame).to be_a(Class)
+	# end
 
 	describe ".initialize" do
 		it "sets @rolls to an empty array" do
@@ -14,16 +14,20 @@ describe BowlingGame do
 	end
 
 	describe "#score" do
-		it "scores the game as 0 when only gutter balls are thrown" do
-			20.times {game.roll(0)}
+		context "when only gutter balls are thrown" do
+			it "scores the game as 0" do
+				20.times {game.roll(0)}
 
-			expect(game.score).to eq(0)
+				expect(game.score).to eq(0)
+			end
 		end
 
-		it "scores the game as 20 when one pin is knocked down on each roll" do
-			20.times {game.roll(1)}
+		context "when one pin is knocked down on each roll" do
+			it "scores the game as 20" do
+				20.times {game.roll(1)}
 
-			expect(game.score).to eq(20)
+				expect(game.score).to eq(20)
+			end
 		end
 	end
 end
