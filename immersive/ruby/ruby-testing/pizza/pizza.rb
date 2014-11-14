135f061f67dc64ee0ed829f3b43d84b2d7310c93
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-13 21:46:39 -0500

    Add code to make Topping test pass

--- immersive/ruby/ruby-testing/pizza/pizza.rb
+++ immersive/ruby/ruby-testing/pizza/pizza.rb
@@ -1,5 +1,11 @@
 module Pizza
+
 	class Pie
+		attr_accessor :toppings
+
+		def initialize(toppings = [])
+			@toppings = toppings
+		end
 	end
 
 	class Topping
@@ -10,4 +16,5 @@ module Pizza
 			@vegetarian = vegetarian
 		end
 	end
+	
 end
