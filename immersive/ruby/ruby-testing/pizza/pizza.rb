132e5ef4f80225ae2f8b17a72a1eb9a1b782a1a2
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-13 21:29:32 -0500

    Add code to make test pass. Add keyword argument 'vegetarian' and attr_accessor

--- immersive/ruby/ruby-testing/pizza/pizza.rb
+++ immersive/ruby/ruby-testing/pizza/pizza.rb
@@ -5,8 +5,9 @@ module Pizza
 	class Topping
 		attr_accessor :name, :vegetarian
 
-		def initialize(name, vegetarian=false)
+		def initialize(name, vegetarian: false)
 			@name = name
+			@vegetarian = vegetarian
 		end
 	end
 end
