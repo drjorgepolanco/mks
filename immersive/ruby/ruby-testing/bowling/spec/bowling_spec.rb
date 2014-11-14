057a81c987fc14805611f2f0939bcc66c032271a
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-14 05:33:55 -0500

    Add failing test to set @rolls as an empty array when initializing

--- immersive/ruby/ruby-testing/bowling/spec/bowling_spec.rb
+++ immersive/ruby/ruby-testing/bowling/spec/bowling_spec.rb
@@ -4,4 +4,12 @@ describe BowlingGame do
 	it "exists" do
 		expect(BowlingGame).to be_a(Class)
 	end
+
+	describe ".initialize" do
+		it "sets @rolls to an empty array" do
+			game = BowlingGame.new
+
+			expect(game.rolls).to eq([])
+		end
+	end
 end
