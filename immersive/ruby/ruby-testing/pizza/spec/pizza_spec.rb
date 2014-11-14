9377f6ab1bfe9a78972bc227fdfd24a0302bca76
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-13 21:06:17 -0500

    First test written. Code to make test pass written too. 'the topping should have a name'

--- immersive/ruby/ruby-testing/pizza/spec/pizza_spec.rb
+++ immersive/ruby/ruby-testing/pizza/spec/pizza_spec.rb
@@ -1,13 +1,17 @@
 require './pizza'
 
-describe Pizza do
-	it "exists" do
-		expect(Pizza).to be_a(Class)
+describe Pizza::Pie do
+	describe '.initialize' do
+
 	end
 end
 
-describe Topping do
-	it "exists" do
-		expect(Topping).to be_a(Class)
+describe Pizza::Topping do
+	describe '.initialize' do
+		it "sets the name of the topping" do
+			topping = Pizza::Topping.new("olives")
+
+			expect(topping.name).to eq("olives")
+		end
 	end
 end
