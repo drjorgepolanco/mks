0a18b5c35e8f3356af96b96929b390637aeda5fd
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-15 03:32:04 -0500

    Add 'create_post' method. First post created succesfully.

--- immersive/http-requests/makerblog.rb
+++ immersive/http-requests/makerblog.rb
@@ -16,14 +16,50 @@ module MakerBlog
 				headers: {"Accept" => "application/json"})
 			posts = response.body
 			posts.each do |post|
-				puts "-" * 10
 				puts "ID: #{post["id"]}"
 				puts "Title: \"#{post["title"]}\" by #{post["name"].split.map(&:capitalize).join(' ')}"
 				puts "Content: #{post["content"]}."
+				puts "-" * 30
 			end
+		end
+
+
+		def show_post(id)
+			url = "http://makerblog.herokuapp.com/posts/#{id}"
+			response = Unirest.get(url,
+				headers: {"Accept" => "application/json"})
+			post = response.body
+			puts "\"#{post["title"]}\": #{post["content"]}."
+		end
+
+		def create_post(name, title, content)
+			url = "http://makerblog.herokuapp.com/posts"
+			payload = {:post => {'name' => name, 'title' => title, 'content' => content}}
+
+			response = Unirest.post(url,
+				headers: {"Accept" => "application/json",
+									"Content-Type" => "application/json"},
+									parameters: payload)
+
+			post = response.body
+			puts "You have succesfully created a new post:"
+			puts "ID: #{post["id"]}"
+			puts "Title: \"#{post["title"]}\" by #{post["name"]}"
+			puts "Content: #{post["content"]}"
 		end
 	end
 end
 
 client = MakerBlog::Client.new
 client.list_posts
+client.show_post(19637)
+
+client.create_post("Kelvinator", "Amazing Title", "Esto se esta poniendo bueno!")
+
+
+
+
+
+
+
+
