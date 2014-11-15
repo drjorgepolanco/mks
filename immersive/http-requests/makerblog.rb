1d4d1bd5186a75f756ac04e5ad15c4c9babcf98b
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-15 03:45:24 -0500

    Add 'edit_post' method. Post succesfully edited.

--- immersive/http-requests/makerblog.rb
+++ immersive/http-requests/makerblog.rb
@@ -29,7 +29,8 @@ module MakerBlog
 			response = Unirest.get(url,
 				headers: {"Accept" => "application/json"})
 			post = response.body
-			puts "\"#{post["title"]}\": #{post["content"]}."
+			puts "\"#{post["title"]}\": #{post["content"]}"
+			puts "_" * 40
 		end
 
 		def create_post(name, title, content)
@@ -46,17 +47,40 @@ module MakerBlog
 			puts "ID: #{post["id"]}"
 			puts "Title: \"#{post["title"]}\" by #{post["name"]}"
 			puts "Content: #{post["content"]}"
+			puts "_" * 40
+		end
+
+		def edit_post(id, options = {})
+			url = "http://makerblog.herokuapp.com/posts/#{id}"
+			params = {}
+
+			params[:name] = options[:name] unless options[:name].nil?
+			params[:title] = options[:title] unless options[:title].nil?
+			params[:content] = options[:content] unless options[:content].nil?
+
+			response = Unirest.put(url,
+				parameters: {:post => params},
+				headers: {"Accept" => "application/json",
+									"Content-Type" => "application/json"})
+
+			post = response.body
+			puts "You have succesfully updated the post:"
+			puts "ID: #{post["id"]}"
+			puts "Title: \"#{post["title"]}\" by #{post["name"]}"
+			puts "Content: #{post["content"]}"
+			puts "_" * 40
 		end
 	end
 end
 
 client = MakerBlog::Client.new
-client.list_posts
+# client.list_posts
+# client.show_post(19637)
+
+# client.create_post("Kelvinator", "Amazing Title", "Esto se esta poniendo bueno!")
+client.edit_post(19637, {name: "Wampam Pimpan", title: "Mustang", content: "Un caballo de carro."})
+
 client.show_post(19637)
-
-client.create_post("Kelvinator", "Amazing Title", "Esto se esta poniendo bueno!")
-
-
 
 
 
