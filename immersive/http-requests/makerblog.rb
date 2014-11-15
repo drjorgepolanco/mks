9bed0d78275d9fac935b4b0fe4a22e1b91d84fba
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-15 04:40:33 -0500

    Encapsulate makerblog url in a variable to have cleaner code

--- immersive/http-requests/makerblog.rb
+++ immersive/http-requests/makerblog.rb
@@ -11,8 +11,13 @@ require 'unirest'
 
 module MakerBlog
 	class Client
+
+		def initialize
+			@makerblog_url = "http://makerblog.herokuapp.com/posts/"
+		end
+
 		def list_posts
-			response = Unirest.get("http://makerblog.herokuapp.com/posts",
+			response = Unirest.get(@makerblog_url,
 				headers: {"Accept" => "application/json"})
 			posts = response.body
 			posts.each do |post|
@@ -20,21 +25,23 @@ module MakerBlog
 				puts "Title: \"#{post["title"]}\" by #{post["name"].split.map(&:capitalize).join(' ')}"
 				puts "Content: #{post["content"]}."
 				puts "-" * 30
+				puts response_code(response.code)
 			end
 		end
 
 
 		def show_post(id)
-			url = "http://makerblog.herokuapp.com/posts/#{id}"
+			url = @makerblog_url + id
 			response = Unirest.get(url,
 				headers: {"Accept" => "application/json"})
 			post = response.body
 			puts "\"#{post["title"]}\": #{post["content"]}"
 			puts "_" * 40
+			puts response_code(response.code)
 		end
 
 		def create_post(name, title, content)
-			url = "http://makerblog.herokuapp.com/posts"
+			url = @makerblog_url
 			payload = {:post => {'name' => name, 'title' => title, 'content' => content}}
 
 			response = Unirest.post(url,
@@ -48,10 +55,11 @@ module MakerBlog
 			puts "Title: \"#{post["title"]}\" by #{post["name"]}"
 			puts "Content: #{post["content"]}"
 			puts "_" * 40
+			puts response_code(response.code)
 		end
 
 		def edit_post(id, options = {})
-			url = "http://makerblog.herokuapp.com/posts/#{id}"
+			url = @makerblog_url + id
 			params = {}
 
 			params[:name] = options[:name] unless options[:name].nil?
@@ -69,28 +77,50 @@ module MakerBlog
 			puts "Title: \"#{post["title"]}\" by #{post["name"]}"
 			puts "Content: #{post["content"]}"
 			puts "_" * 40
+			puts response_code(response.code)
 		end
 
 		def delete_post(id)
-			url = "http://makerblog.herokuapp.com/posts/#{id}"
+			url = @makerblog_url + id
 			response = Unirest.delete(url, 
 				headers: {"Accept" => "application/json"})
-			puts response.code
+			puts response_code(response.code)
 			puts "You have succesfully deleted the post #{id}."
+		end
+
+		private
+
+		def response_code(code)
+			code = code.to_s
+
+			case code[0]
+				when '1'
+					puts "Informational"
+				when '2'
+					puts "Success"
+				when '3'
+					puts "Redirection"
+				when '4'
+					puts "Client Error"
+				when '5'
+					puts "Server Error"
+				else
+					puts "Unknown"
+			end
 		end
 	end
 end
 
 client = MakerBlog::Client.new
 client.list_posts
-client.show_post(19637)
+# client.show_post(19682)
 
-client.create_post("Kelvinator", "Amazing Title", "Esto se esta poniendo bueno!")
-client.edit_post(19637, {name: "Wampam Pimpan", title: "Mustang", content: "Un caballo de carro."})
+# client.create_post("Kelvinator", "Amazing Title", "Esto se esta poniendo bueno!")
+# client.edit_post(19682, {name: "Wampam Pimpan", title: "Mustang", content: "Un caballo de carro."})
 
-client.show_post(19637)
+# client.show_post(19637)
 
-client.delete_post(19637)
+# client.delete_post(19682)
 
 
 
