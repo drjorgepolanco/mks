15cd5107300b1ff53a8282f39d1c987e2883cacf
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-14 22:22:13 -0500

    Improve code and messages to look better formatted and make more sense.

--- immersive_prework/ruby/cars_app/cars_app.rb
+++ immersive_prework/ruby/cars_app/cars_app.rb
@@ -1,26 +1,7 @@
 class Car
 	@@total_car_count = 0
 	@@cars_per_color = {}
-	attr_accessor :color
-
-	def self.total_car_count
-		@@total_car_count
-	end
-
-	def to_s()
-		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
-	end
-
-	def self.show_cars
-		@@cars_per_color.each do |key, value|
-			puts "#{key}: #{value}"
-		end
-	end
-
-	def self.most_popular_color
-		most_popular = @@cars_per_color.values.max
-		keys = @@cars_per_color.select {|key, value| value == most_popular}.keys
-	end
+	attr_accessor :color, :fuel
 
 	def initialize(color = "white")
 		@@total_car_count += 1
@@ -34,6 +15,36 @@ class Car
 		end
 	end
 
+
+	def self.total_car_count
+		@@total_car_count
+		puts "We have #{@@total_car_count} cars in stock."
+	end
+
+	def self.show_cars
+		puts "We have the following cars available:"
+		@@cars_per_color.each do |key, value|
+			if @@cars_per_color[key] == 1
+				puts "#{key}: #{value} car."
+			else
+				puts "#{key}: #{value} cars."
+			end
+		end
+		puts " "
+	end
+
+	def self.most_popular_color
+		most_popular = @@cars_per_color.values.max
+		keys = @@cars_per_color.select {|key, value| value == most_popular}.keys
+		puts "Our most popular car colors are: " + keys[0..-2].map(&:capitalize).join(", ") + " and " + keys[-1].capitalize + "." if keys.length > 1 
+		puts "Our most popular car color is: " + keys[0].capitalize + "." unless keys.length > 1
+		puts " "
+	end
+
+	def to_s
+		"I'm a car! I've driven #{@distance} miles and have #{@fuel} gallons gas left"
+	end
+
 	def drive(miles)
 		if (@fuel - miles/20.0) >= 0
 			@distance += miles
@@ -45,10 +56,12 @@ class Car
 		end
 	end
 
-	def fuel_up()
+	def fuel_up
 		gallons_needed = 10.0 - @fuel
-		puts "You must pay $#{3.5 * gallons_needed}"
+		puts "To fill the tank you need #{gallons_needed} gallons."
+		puts "That will cost you $#{3.5 * gallons_needed} dollars."
 		@fuel = 10.0
+		puts "You have now #{@fuel} gallons and can drive #{@fuel * 20} more miles."
 	end
 
 	def add_car_color
@@ -72,6 +85,8 @@ class Car
 	end
 end
 
+
+
 class Convertible < Car
 	attr_accessor :roof_status
 
@@ -81,21 +96,21 @@ class Convertible < Car
 		@roof_status = "closed"
 	end
 
-	def top_down
+	def top_up
 		if @roof_status == "closed"
 			@roof_status = "open"
-			puts "Now opening the roof"
+			puts "Now opening the roof."
 		else
-			puts "The roof was already open"
+			puts "The roof was already open."
 		end
 	end
 
 	def top_down
 		if @roof_status == "open"
 			@roof_status = "closed"
-			puts "Now closing the roof"
+			puts "Now closing the roof."
 		else
-			puts "The roof was already closed"
+			puts "The roof was already closed."
 		end
 	end
 end
--- immersive_prework/ruby/cars_app/testcode.rb
+++ immersive_prework/ruby/cars_app/testcode.rb
@@ -4,48 +4,46 @@ require_relative 'cars_app'
 car_1 = Car.new("blue")
 car_2 = Car.new("red")
 car_3 = Car.new("blue")
-car_4 = Car.new("yellow")
+car_4 = Car.new("white")
+car_5 = Car.new("yellow")
+car_6 = Car.new("blue")
+car_7 = Car.new("red")
+car_8 = Car.new("black")
+car_9 = Car.new("yellow")
+car_10 = Car.new("black")
 
+
+puts Car.total_car_count
 Car.show_cars
+puts Car.most_popular_color
+
 
 car_3.color = "black"
-puts car_3.color
-
+car_5.color = "blue"
 Car.show_cars
-
-car_5 = Car.new("black")
-
-puts Car.show_cars
-
 puts Car.most_popular_color
 
 
 car_x = Convertible.new("red")
 car_y = Convertible.new("yellow")
 
-puts car_x.roof_status
+puts "The top is currently " + car_x.roof_status + "."
 
-car_x.roof_status = "open"
+car_x.top_up
 
-puts car_x.roof_status
+puts "The top is currently " + car_x.roof_status + "."
 
 car_x.top_down
 car_x.top_down
 
 
-# car_a = Car.new()
-# car_b = Car.new()
-# puts car_a
-# puts car_b
-# car_a.drive(10)
-# puts car_a
-# puts car_b
-# car_a.drive(232)exit
-# car_b.drive(117)
-# puts car_a
-# puts car_b
+car_a = Car.new()
+car_b = Car.new()
 
+car_a.drive(150)
+car_b.drive(77)
+puts car_a
+puts car_b
 
-
-
-# load 'testcode.rb'
+car_a.fuel_up
+car_a.fuel
