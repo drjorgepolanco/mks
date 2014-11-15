0924252e7bdc531637e146627a636e16436610e2
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-15 05:29:49 -0500

    Add a user interface, separate code from user interface and put both files in a directory.

---
+++ immersive/http-requests/makerblog/makerblog.rb
@@ -0,0 +1,112 @@
+require 'unirest'
+
+# To verify there is an actual communication. Returns 200 if everything went fine.
+# --------------------------------------------------------------------------------
+
+# response = Unirest.get('http://makerblog.herokuapp.com/posts',
+# 	headers: {"Accept" => "application/json"})
+
+# puts response
+# puts response.code
+
+module MakerBlog
+	class Client
+
+		def initialize
+			@makerblog_url = "http://makerblog.herokuapp.com/posts/"
+		end
+
+		def list_posts
+			response = Unirest.get(@makerblog_url,
+				headers: {"Accept" => "application/json"})
+			posts = response.body
+			posts.each do |post|
+				puts "ID: #{post["id"]}"
+				puts "Title: \"#{post["title"]}\" by #{post["name"].split.map(&:capitalize).join(' ')}"
+				puts "Content: #{post["content"]}."
+				puts "-" * 30
+				puts response_code(response.code)
+			end
+		end
+
+
+		def show_post(id)
+			url = @makerblog_url + id
+			response = Unirest.get(url,
+				headers: {"Accept" => "application/json"})
+			post = response.body
+			puts "\"#{post["title"]}\": #{post["content"]}"
+			puts "_" * 40
+			puts response_code(response.code)
+		end
+
+		def create_post(name, title, content)
+			url = @makerblog_url
+			payload = {:post => {'name' => name, 'title' => title, 'content' => content}}
+
+			response = Unirest.post(url,
+				headers: {"Accept" => "application/json",
+									"Content-Type" => "application/json"},
+									parameters: payload)
+
+			post = response.body
+			puts "You have succesfully created a new post:"
+			puts "ID: #{post["id"]}"
+			puts "Title: \"#{post["title"]}\" by #{post["name"]}"
+			puts "Content: #{post["content"]}"
+			puts "_" * 40
+			puts response_code(response.code)
+		end
+
+		def edit_post(id, options = {})
+			url = @makerblog_url + id
+			params = {}
+
+			params[:name] = options[:name] unless options[:name].nil?
+			params[:title] = options[:title] unless options[:title].nil?
+			params[:content] = options[:content] unless options[:content].nil?
+
+			response = Unirest.put(url,
+				parameters: {:post => params},
+				headers: {"Accept" => "application/json",
+									"Content-Type" => "application/json"})
+
+			post = response.body
+			puts "You have succesfully updated the post:"
+			puts "ID: #{post["id"]}"
+			puts "Title: \"#{post["title"]}\" by #{post["name"]}"
+			puts "Content: #{post["content"]}"
+			puts "_" * 40
+			puts response_code(response.code)
+		end
+
+		def delete_post(id)
+			url = @makerblog_url + id
+			response = Unirest.delete(url, 
+				headers: {"Accept" => "application/json"})
+			puts response_code(response.code)
+			puts "You have succesfully deleted the post #{id}."
+		end
+
+		private
+
+		def response_code(code)
+			code = code.to_s
+
+			case code[0]
+				when '1'
+					puts "Informational"
+				when '2'
+					puts "Success"
+				when '3'
+					puts "Redirection"
+				when '4'
+					puts "Client Error"
+				when '5'
+					puts "Server Error"
+				else
+					puts "Unknown"
+			end
+		end
+	end
+end
---
+++ immersive/http-requests/makerblog/makerblog_user_interface.rb
@@ -0,0 +1,54 @@
+require_relative 'makerblog'
+
+client = MakerBlog::Client.new
+
+puts "MakerBlog: The ultimate Post creating machine!"
+puts "To get started, write what you want to do."
+
+user_choice = puts "Please choose between: all, create, read, update and delete:"
+user_choice = gets.chomp.downcase
+
+case user_choice
+when 'all'
+  client.list_posts
+when 'create'
+  user_name = puts "Please, write your name:"
+  user_name = gets.chomp.split.map(&:capitalize).join(' ')
+  user_title = puts "Please, write a title for your Post:"
+  user_title = gets.chomp.split.map(&:capitalize).join(' ')
+  user_content = puts "Please, write the content of your Post:"
+  user_content = gets.chomp
+
+  client.create_post(user_name, user_title, user_content)
+when 'read'
+  user_post_id = puts "Please, provide the id of the post you want to read: "
+  user_post_id = gets.chomp
+
+  client.show_post(user_post_id)
+when 'update'
+  user_post_id = puts "Please, provide the id of the post you want to edit: "
+  user_post_id = gets.chomp
+  user_name = puts "Please, write a new name for your Post. Leave empty if your don't want to change it:"
+  user_name = gets.chomp.split.map(&:capitalize).join(' ')
+  user_title = puts "Please, write a new title for your Post. Leave empty if your dont want to change it:"
+  user_title = gets.chomp.split.map(&:capitalize).join(' ')
+  user_content = puts "Please, write a new content for your Post. Leave empty if your dont want to change it:"
+  user_content = gets.chomp
+
+  client.edit_post(user_post_id, {name: user_name, title: user_title, content: user_content})
+when 'delete'
+  user_post_id = puts "Please, provide the id of the post you want to delete: "
+  user_post_id = gets.chomp
+
+  client.delete_post(user_post_id)
+else
+  puts "Unknown command"
+  user_choice
+end
+
+# client.list_posts
+# client.show_post(19682)
+# client.create_post("Kelvinator", "Amazing Title", "Esto se esta poniendo bueno!")
+# client.edit_post(19682, {name: "Wampam Pimpan", title: "Mustang", content: "Un caballo de carro."})
+# client.show_post(19637)
+# client.delete_post(19682)
