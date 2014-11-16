7c07c00b2d23d60f8d1f759b7b6653ddf8a2c691
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-16 05:56:30 -0500

    Add code to test 'followers' method. Output: both users's count.

--- immersive/apis/follower-clash/follower_clash.rb
+++ immersive/apis/follower-clash/follower_clash.rb
@@ -1,1 +1,6 @@
 require_relative 'lib/user_comparer'
+
+user1 = UserComparer::User.new('drjorgepolanco')
+user2 = UserComparer::User.new('maximashi')
+puts "#{user1.username}'s follower count: #{user1.followers}"
+puts "#{user2.username}'s follower count: #{user2.followers}"
