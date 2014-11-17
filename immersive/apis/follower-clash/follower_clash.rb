e464af0c52a2c6d4f6aad6a6bce8a792f25a44f7
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-16 21:25:12 -0500

    Add method 'friends' to find out the amount of users a user is following, abstract client to initialize to DRY code

--- immersive/apis/follower-clash/follower_clash.rb
+++ immersive/apis/follower-clash/follower_clash.rb
@@ -8,3 +8,6 @@ puts "#{user2.username}'s follower count: #{user2.followers}"
 comparer = UserComparer::Comparer.new(user1, user2)
 
 puts "The most popular user is: #{comparer.compare_followers}"
+
+puts "#{user1.username} is following #{user1.friends} users."
+puts "#{user2.username} is following #{user2.friends} users."
