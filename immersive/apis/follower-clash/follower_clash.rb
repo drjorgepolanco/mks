a785ef5007fcb5e149c4b065f5382a1686fee250
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-16 21:18:20 -0500

    add code to compare users and find the most popular one

--- immersive/apis/follower-clash/follower_clash.rb
+++ immersive/apis/follower-clash/follower_clash.rb
@@ -4,3 +4,7 @@ user1 = UserComparer::User.new('drjorgepolanco')
 user2 = UserComparer::User.new('maximashi')
 puts "#{user1.username}'s follower count: #{user1.followers}"
 puts "#{user2.username}'s follower count: #{user2.followers}"
+
+comparer = UserComparer::Comparer.new(user1, user2)
+
+puts "The most popular user is: #{comparer.compare_followers}"
