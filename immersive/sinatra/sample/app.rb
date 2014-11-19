b99a205c410ea824ed6bf7ea07d90963c870d110
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-19 01:42:50 -0500

    Playing with params to pass the information gathered in the form in views/gather-info to views/summary.

--- immersive/sinatra/sample/app.rb
+++ immersive/sinatra/sample/app.rb
@@ -11,3 +11,12 @@ end
 get '/gather-info' do
 	erb :info
 end
+
+post '/info-summary' do
+	puts params
+	@name = params[:name]
+	@email = params[:mail]
+	@vegetarian = params[:vegetarian]
+	@blood_type = params[:bloodtype]
+	erb :summary
+end
