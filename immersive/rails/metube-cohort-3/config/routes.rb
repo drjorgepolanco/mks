48e29570d17165a03c890ae06cb97f414810fbaf
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-21 02:15:14 -0500

    Add route to display two videos on one page.

--- immersive/rails/metube-cohort-3/config/routes.rb
+++ immersive/rails/metube-cohort-3/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "videos#show_all"
   get '/videos' => 'videos#show_all', as: 'videos'
   get '/videos/:video_name' => 'videos#show', as: 'video'
+  get 'videos/:video_one_name/:video_two_name' => 'videos#show_two', as: 'show_two'
 
   # get '/videos/gladiator' => 'videos#show_gladiator', as: 'gladiator_video'
   # get '/videos/matilda' => 'videos#show_matilda', as: 'matilda_video'
