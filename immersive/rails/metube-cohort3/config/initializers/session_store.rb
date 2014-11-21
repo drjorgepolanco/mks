666b35139dbf03936724c000cbc652cdf6dd875a
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-20 20:29:43 -0500

    Delete mks metube because I was not able to push back my commits.

--- immersive/rails/metube-cohort3/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-MetubeCohort3::Application.config.session_store :cookie_store, key: '_metube_cohort3_session'
--- immersive/rails/metube-cohort3/config/routes.rb
+++
@@ -1,6 +0,0 @@
-MetubeCohort3::Application.routes.draw do
-
-  get '/movies/gladiator' => 'movies#show_gladiator'
-  get '/movies/matilda' => 'movies#show_matilda'
-  
-end
