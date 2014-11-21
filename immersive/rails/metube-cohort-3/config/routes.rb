c1e5f18ecedb4e02ba5953832ff798aeff9c971d
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-21 00:08:49 -0500

    Add route to show_all

--- immersive/rails/metube-cohort-3/config/routes.rb
+++ immersive/rails/metube-cohort-3/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/videos' => 'videos#show_all'
+
   get '/videos/gladiator' => 'videos#show_gladiator'
   get '/videos/matilda' => 'videos#show_matilda'
   get '/videos/seinfeld' => 'videos#show_seinfeld'
