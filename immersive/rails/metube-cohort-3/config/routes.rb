c418bbe51429ff50e4ad7ea49a562e248079482e
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-21 00:30:40 -0500

    Update routes to use 'named route'

--- immersive/rails/metube-cohort-3/config/routes.rb
+++ immersive/rails/metube-cohort-3/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   get '/videos' => 'videos#show_all'
 
-  get '/videos/gladiator' => 'videos#show_gladiator'
-  get '/videos/matilda' => 'videos#show_matilda'
-  get '/videos/seinfeld' => 'videos#show_seinfeld'
-  get '/videos/friends' => 'videos#show_friends'
+  get '/videos/gladiator' => 'videos#show_gladiator', as: 'gladiator_video'
+  get '/videos/matilda' => 'videos#show_matilda', as: 'matilda_video'
+  get '/videos/seinfeld' => 'videos#show_seinfeld', as: 'seinfeld_video'
+  get '/videos/friends' => 'videos#show_friends', as: 'friends_video'
 end
