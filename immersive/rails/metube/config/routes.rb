dde98a3c689226a8d96042c97f2975cc8ab8f9e6
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-20 22:10:02 -0500

    Add routes to shows

--- immersive/rails/metube/config/routes.rb
+++ immersive/rails/metube/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get "/movies/gladiator" => "movies#show_gladiator"
   get "/movies/matilda" => "movies#show_matilda"
   get '/movies/batman' => 'movies#show_batman'
-  get 'movies/superman' => 'movies#show_superman'
+  get '/movies/superman' => 'movies#show_superman'
+  get '/shows/seinfeld' => 'shows#show_seinfeld'
+  get '/shows/friends' => 'shows#show_friends'
+  get '/shows/senora_acero' => 'shows#show_senora_acero'
 end
