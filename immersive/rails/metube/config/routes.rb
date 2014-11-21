943bb3ec1265f59bb552e3968f7fd2e1a5399d39
Jorge Polanco <dr.jorgepolanco@gmail.com>
2014-11-20 20:33:21 -0500

    Add routes for batman and superman

--- immersive/rails/metube/config/routes.rb
+++ immersive/rails/metube/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/movies/gladiator" => "movies#show_gladiator"
   get "/movies/matilda" => "movies#show_matilda"
+  get '/movies/batman' => 'movies#show_batman'
+  get 'movies/superman' => 'movies#show_superman'
 end
