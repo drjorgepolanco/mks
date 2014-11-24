eb9366041254eba140c6b5e7b18fd8f2f77c91c8
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-24 02:32:45 -0500

    Add route for method create

--- immersive/rails/metube/metube-5/config/routes.rb
+++ immersive/rails/metube/metube-5/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'videos#show_all'
   get '/videos/new' => 'videos#new', as: 'new_video'
+  post '/videos' => 'videos#create'
   get '/videos' => 'videos#show_all'
   get '/videos/:id' => 'videos#show', as: 'video'
 end
