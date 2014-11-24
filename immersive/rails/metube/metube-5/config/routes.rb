7fa910fcff817caa714a34f2b59642f029db0c5d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-24 02:22:23 -0500

    Add routes to new video:

--- immersive/rails/metube/metube-5/config/routes.rb
+++ immersive/rails/metube/metube-5/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'videos#show_all'
+  get '/videos/new' => 'videos#new', as: 'new_video'
   get '/videos' => 'videos#show_all'
   get '/videos/:id' => 'videos#show', as: 'video'
 end
