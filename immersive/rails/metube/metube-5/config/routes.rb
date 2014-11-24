f1b4ff6e8f263b1ca4be24fb749a0c82ac925fed
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-24 10:45:33 -0500

    Add routes for Update

--- immersive/rails/metube/metube-5/config/routes.rb
+++ immersive/rails/metube/metube-5/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'videos#show_all'
-  get '/videos/new' => 'videos#new', as: 'new_video'
-  post '/videos' => 'videos#create'
-  get '/videos' => 'videos#show_all'
-  get '/videos/:id' => 'videos#show', as: 'video'
+  root  'videos#show_all'
+  get   '/videos'           => 'videos#show_all'
+  get   '/videos/new'       => 'videos#new',  as: 'new_video'
+  get   '/videos/:id'       => 'videos#show', as: 'video'
+  get   '/videos/:id/edit'  => 'videos#edit', as: 'edit_video'
+  post  '/videos'           => 'videos#create'
+  put   '/videos/:id'       => 'videos#update'
+  patch '/videos/:id'       => 'videos#update'
 end
