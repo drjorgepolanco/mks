b18c7b5d3e6da99ab8600c2a276dc057c45badeb
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-26 07:36:21 -0500

    Add method full_name, fix bug from @alumni to self

--- immersive/rails/makerbook/app/models/alumni.rb
+++ immersive/rails/makerbook/app/models/alumni.rb
@@ -1,6 +1,6 @@
 class Alumni < ActiveRecord::Base
 
 	def full_name
-		"#{@alumni.first_name.capitalize} #{@alumni.last_name.capitalize}"
+		"#{self.first_name.capitalize} #{self.last_name.capitalize}"
 	end
 end
