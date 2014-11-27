1150c23ab111688d3e888565b816451ba2a59454
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-26 21:23:31 -0500

    Add length validation to first and last name to 50 characters

--- immersive/rails/metube/metube-6/app/models/user.rb
+++ immersive/rails/metube/metube-6/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :videos
 	before_save { self.email_address = email_address.downcase }
-	validates :first_name, :last_name, presence: true
+	validates :first_name, :last_name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email_address, presence: true, length: { maximum: 255 },
 														format: { with: VALID_EMAIL_REGEX },
