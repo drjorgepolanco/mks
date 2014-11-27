6d9cf99f3a5db7af79de8e7a86f37f61cb7611eb
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-27 10:44:41 -0500

    Add validations to user model

--- immersive/rails/metube/metube-7/app/models/user.rb
+++ immersive/rails/metube/metube-7/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :videos
 
+	validates :first_name, :last_name, presence: true
+	validates :email_address, presence: true, uniqueness: true
+
 	def full_name
 		"#{first_name} #{last_name}"
 	end
