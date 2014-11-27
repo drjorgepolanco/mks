abb575806de8ec3012c10da1d6efef9f284d18f3
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-27 05:37:21 -0500

    7th Installment of Metube. Intro to Test Driven Development. Start code match branch 'add-ratings' of makersquare/metube-cohort3. Initial commit.

---
+++ immersive/rails/metube/metube-7/app/controllers/logins_controller.rb
@@ -0,0 +1,23 @@
+class LoginsController < ApplicationController
+	def new
+	end
+
+	def create
+		email = params[:email_address]
+		user = User.find_by_email(email)
+		unless user
+			redirect_to signin_path
+			flash[:notice] = "Please enter an existing user's email."
+		else
+			session[:current_user_id] = user.id
+			flash[:notice] = "You have successfully logged in!"
+			redirect_to root_url
+		end
+	end
+
+	def destroy
+		@_current_user = session[:current_user_id] = nil
+		flash[:notice] = "You have successfully logged out."
+		redirect_to root_url
+	end
+end
