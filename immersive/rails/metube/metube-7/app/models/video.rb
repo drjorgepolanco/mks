e77c10f50102cb338c94ee53da1533c8b244e530
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-27 11:44:07 -0500

    Refactor using the ternary operator

--- immersive/rails/metube/metube-7/app/models/video.rb
+++ immersive/rails/metube/metube-7/app/models/video.rb
@@ -9,10 +9,11 @@ class Video < ActiveRecord::Base
 		self.ratings.to_a.each do |rating|
 			total += rating.value
 		end
-		if self.ratings.count == 0
-			return 0
-		else
-			return total / self.ratings.count
-		end
+		self.ratings.count == 0 ? 0 : total / self.ratings.count
+		# if self.ratings.count == 0
+		# 	return 0
+		# else
+		# 	return total / self.ratings.count
+		# end
 	end
 end
