9af38d8f21d6a1c0f34f1311e8d93deb92f02f05
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-27 11:37:40 -0500

    Add method 'avg_rating' to Video

--- immersive/rails/metube/metube-7/app/models/video.rb
+++ immersive/rails/metube/metube-7/app/models/video.rb
@@ -3,4 +3,12 @@ class Video < ActiveRecord::Base
 	has_many :ratings
 
 	validates :title, :youtube_id, :user_id, presence: true
+
+	def avg_rating
+		total = 0.0
+		self.ratings.to_a.each do |rating|
+			total += rating.value
+		end
+		total / self.ratings.count
+	end
 end
