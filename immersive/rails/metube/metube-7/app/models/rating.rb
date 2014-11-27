e008fd17e0290686640fc32f8bc6d1b98555b380
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-27 11:22:28 -0500

    Add inclusion validation to Rating to ensure the input value is within 1 and 5

--- immersive/rails/metube/metube-7/app/models/rating.rb
+++ immersive/rails/metube/metube-7/app/models/rating.rb
@@ -4,4 +4,5 @@ class Rating < ActiveRecord::Base
 
 	validates :value, :user_id, :video_id, presence: true
 	validates :video_id, uniqueness: { scope: :user_id }
+	validates :value, inclusion: { in: 1..5, message: "must be between 1 and 5" }
 end
