f187377072a166edaee6482ab53d71390478eb4f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-26 21:31:25 -0500

    Add password_digest to Users table

--- immersive/rails/metube/metube-6/db/schema.rb
+++ immersive/rails/metube/metube-6/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141127021243) do
+ActiveRecord::Schema.define(version: 20141127023012) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141127021243) do
     t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email_address"], name: "index_users_on_email_address", unique: true
