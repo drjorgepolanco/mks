840b77924bb7e36fc79d3cf2057f172f32e79142
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-26 21:09:23 -0500

    Add email validation for length and format

--- immersive/rails/metube/metube-6/app/models/user.rb
+++ immersive/rails/metube/metube-6/app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
 	has_many :videos
 
 	validates :first_name, :last_name, presence: true
-	validates :email_address, presence: true, uniqueness: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email_address, presence: true, length: { maximum: 255 },
+														format: { with: VALID_EMAIL_REGEX },
+														uniqueness: { case_sensitive: false }
 end
