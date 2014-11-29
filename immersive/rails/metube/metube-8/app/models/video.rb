34c2ef42af21fb8273f68c33cbe43ddc1410ed8c
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-29 13:19:57 -0500

    Add after_create model callback to set_description

--- immersive/rails/metube/metube-8/app/models/video.rb
+++ immersive/rails/metube/metube-8/app/models/video.rb
@@ -1,4 +1,11 @@
 class Video < ActiveRecord::Base
 	belongs_to :user
   validates :user_id, :youtube_id, :title, presence: true
+
+  after_create :set_description
+
+  def set_description
+	  self.description = "This is a test to make sure a description is created immediately after a video record is created"
+	  self.save
+	end
 end
