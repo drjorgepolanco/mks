be1adb46ccd39f03d7402f8c9c60b656de8a64cf
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-30 04:28:08 -0500

    Remove showtimes from movie

--- immersive/rails/urdb/urdb-2/db/schema.rb
+++ immersive/rails/urdb/urdb-2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130092256) do
+ActiveRecord::Schema.define(version: 20141130092711) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20141130092256) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "showtimes"
   end
 
 end
