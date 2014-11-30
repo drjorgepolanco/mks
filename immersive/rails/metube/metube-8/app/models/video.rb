ea96d894d31cd6712aec362eab8cb7bc598543a3
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-29 22:28:04 -0500

    Add code to pull video title

--- immersive/rails/metube/metube-8/app/models/video.rb
+++ immersive/rails/metube/metube-8/app/models/video.rb
@@ -1,12 +1,14 @@
 class Video < ActiveRecord::Base
 	belongs_to :user
-  validates :user_id, :youtube_id, :title, presence: true
+  validates :user_id, :youtube_id, presence: true
 
   after_create :youtube_query
 
   def youtube_query
 	  response = Unirest.get("https://www.googleapis.com/youtube/v3/videos?id=#{self.youtube_id}&key=API_KEY&part=snippet")
+	  self.title = response.body["items"][0]['snippet']['title']
     self.description = response.body["items"][0]['snippet']['description']
     self.save
 	end
 end
+
