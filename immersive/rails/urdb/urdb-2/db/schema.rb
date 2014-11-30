acaafb19e0a2705131dca8e48e9b15c2a6d23323
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-30 04:29:38 -0500

    Create model Showtime

--- immersive/rails/urdb/urdb-2/db/schema.rb
+++ immersive/rails/urdb/urdb-2/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130092711) do
+ActiveRecord::Schema.define(version: 20141130092907) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
     t.string   "image"
     t.string   "trailer"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "showtimes", force: true do |t|
+    t.string   "location"
+    t.string   "time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
