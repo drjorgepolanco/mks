e537741b63577ff8987975ba4b1b1353cbba939c
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-29 23:36:36 -0500

    Add environment variable for API_KEY

--- immersive/rails/metube/metube-8/app/models/video.rb
+++ immersive/rails/metube/metube-8/app/models/video.rb
@@ -5,10 +5,9 @@ class Video < ActiveRecord::Base
   after_create :youtube_query
 
   def youtube_query
-	  response = Unirest.get("https://www.googleapis.com/youtube/v3/videos?id=#{self.youtube_id}&key=API_KEY&part=snippet")
+	  response = Unirest.get("https://www.googleapis.com/youtube/v3/videos?id=#{self.youtube_id}&key=#{ENV['API_KEY']}&part=snippet")
 	  self.title = response.body["items"][0]['snippet']['title']
     self.description = response.body["items"][0]['snippet']['description']
     self.save
 	end
 end
-
