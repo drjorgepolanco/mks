a830722a7ba9b991fe6d7db07ff73758511e9f44
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-11-29 21:03:51 -0500

    Update URL to match new Youtube API's version, fix bug in method name, add code in url to pull description dynamically.

--- immersive/rails/metube/metube-8/app/models/video.rb
+++ immersive/rails/metube/metube-8/app/models/video.rb
@@ -4,9 +4,9 @@ class Video < ActiveRecord::Base
 
   after_create :youtube_query
 
-  def set_description
-	  response = Unirest.get("https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id=020sZ0PY8ko&key={API_TOKEN}")
-		self.description = response.body["items"][0]['snippet']['description']
-		self.save
+  def youtube_query
+	  response = Unirest.get("https://www.googleapis.com/youtube/v3/videos?id=#{self.youtube_id}&key=API_KEY&part=snippet")
+    self.description = response.body["items"][0]['snippet']['description']
+    self.save
 	end
 end
