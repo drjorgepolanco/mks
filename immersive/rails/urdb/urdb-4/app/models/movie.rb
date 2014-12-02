c8ca883d8c6aac0268e462d0c34d6610f6ffafc4
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-02 10:56:05 -0500

    Add method generate_slug, slug validation and call to generate slug before validation

--- immersive/rails/urdb/urdb-4/app/models/movie.rb
+++ immersive/rails/urdb/urdb-4/app/models/movie.rb
@@ -3,12 +3,24 @@ class Movie < ActiveRecord::Base
 	has_many :comments
 	has_many :roles
 	has_many :stars, :through => :roles
+
+	validates :slug, uniqueness: true, presence: true
+
+	before_validation :generate_slug
 	
 	def snippet
     self.description.truncate 50
   end
 
   def to_param
-  	"#{id} - #{title}".parameterize
+  	slug
   end
+
+  def generate_slug
+  	self.slug ||= title.parameterize
+  end
+
+  # def to_param
+  # 	"#{id} - #{title}".parameterize
+  # end
 end
