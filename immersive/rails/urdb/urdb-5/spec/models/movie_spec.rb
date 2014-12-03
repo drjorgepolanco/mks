21e4a175a3263f3bbdb05f07d54e035e18353d3d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-03 11:08:17 -0500

    Add test for when all movies can be found on Rotten Tomatoes

--- immersive/rails/urdb/urdb-5/spec/models/movie_spec.rb
+++ immersive/rails/urdb/urdb-5/spec/models/movie_spec.rb
@@ -47,4 +47,30 @@ RSpec.describe Movie, :type => :model do
   		end
   	end
   end
+
+  describe ".average_rating" do
+  	let(:movie2) { Movie.new }
+
+  	context "when all movies can be found on Rotten Tomatoes" do
+  		it "returns the average audience_score for all movies" do
+  			allow(movie).to receive(:audience_rating).and_return(100)
+  			allow(movie2).to receive(:audience_rating).and_return(50)
+  			allow(Movie).to receive(:all).and_return([movie, movie2])
+
+  			expect(Movie.average_rating).to eq(75.0)
+  		end
+  	end
+
+  	context "when some movies are not found on Rotten Tomatoes" do
+  		it "returns the average audience_score of the movies that where found" do
+
+  		end
+  	end
+
+  	context "when no movies are found on Rotten Tomatoes" do
+  		it "returns nil" do
+  		end
+  	end
+
+  end
 end
