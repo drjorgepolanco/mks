b7b21e2e354e3adb035acfcd587cb6e40eddc5fd
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-03 11:07:52 -0500

    Create class method average_rating to calculate the average of the audience ratings of the movies

--- immersive/rails/urdb/urdb-5/app/models/movie.rb
+++ immersive/rails/urdb/urdb-5/app/models/movie.rb
@@ -3,10 +3,31 @@ class Movie < ActiveRecord::Base
 	has_many :roles
 	has_many :stars, through: :roles
 
-	validates :title, :poster_url, presence: true
+	validates :title, presence: true
 
 	def rotten_finder
     RottenMovie.find(title: title, limit: 1)
+  end
+
+  def audience_rating
+  	rotten_finder.ratings.audience_score unless rotten_finder.empty?
+  end
+
+  def self.average_rating
+  	# Get all the movie scores and store them in an array
+  	scores = self.all.collect do |movie|
+  		movie.audience_rating
+  	end
+
+  	# Add all the movie scores in the array together
+  	scores.compact!
+
+  	# Divide by the number of movies
+  	if scores.length > 0
+  		scores.sum / scores.length
+  	else
+  		nil
+  	end
   end
   
   def snippet
