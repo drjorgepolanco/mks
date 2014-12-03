cd0846a3e257e2c161f274d7c1e73fb3ff97cfb4
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-03 10:15:39 -0500

    Add tests for rotten_finder method

--- immersive/rails/urdb/urdb-5/spec/models/movie_spec.rb
+++ immersive/rails/urdb/urdb-5/spec/models/movie_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Movie, :type => :model do
   let(:movie) { Movie.new }
+  let(:rotten_finder_return) { OpenStruct.new(ratings: OpenStruct.new(audience_score: 84)) }
 
   describe "#snippet" do
   	context "when description is less than or equal to 50 characters" do
@@ -25,6 +26,24 @@ RSpec.describe Movie, :type => :model do
   			allow(movie).to receive(:description).and_return(nil)
 
   			expect(movie.snippet).to eq("")
+  		end
+  	end
+  end
+
+  describe "#audience_rating" do
+  	context "when Movie is found on Rotten Tomatoes" do
+  		it "allows the audience score for the Movie" do
+  			allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+  			expect(movie.audience_rating).to eq(84)
+  		end
+  	end
+
+  	context "when Movie is not found on Rotten Tomatoes" do
+  		it "returns an empty string" do
+  			allow(movie).to receive(:rotten_finder).and_return("")
+
+  			expect(movie.audience_rating).to eq(nil)
   		end
   	end
   end
