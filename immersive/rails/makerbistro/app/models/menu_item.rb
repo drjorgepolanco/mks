7d90c06666a4c9ceec7babc1f3165731198a66a1
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-04 22:53:20 -0500

    Add default_scope DESC, add uniqueness validation to :name

--- immersive/rails/makerbistro/app/models/menu_item.rb
+++ immersive/rails/makerbistro/app/models/menu_item.rb
@@ -2,5 +2,8 @@ class MenuItem < ActiveRecord::Base
 	has_many :recipes
 	has_many :ingredients, through: :recipes
 
-	validates :name, :image, presence: true
+	default_scope -> { order('created_at DESC') }
+
+	validates :name, presence: true, uniqueness: true
+	validates :image, presence: true
 end
