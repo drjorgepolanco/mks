1937701732316f2bf6e9e1cbf209db621ec87c5d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-06 10:28:26 -0500

    Library of Books representation with Hash

---
+++ software_engineering/data_structure_modeling_1/library_of_books/library_of_books.rb
@@ -0,0 +1,22 @@
+books = {
+	:gourd_of_flies => {
+		:name => "Gourd of the Flies",
+		:author => "J. W. Gourding"
+	},
+	:design_patterns => {
+		:name => "Design Patterns",
+		:author => ["John Vlissides", "Richard Helm", "Ralph Johnson", "Erich Gamma"]
+	},
+	:enterprise_app_architecture => {
+		:name => "Patterns of Enterprise Application Architecture",
+		:author => "Martin Fowler"
+	},
+	:domain_driven_design => {
+		:name => "Domain Driven Design",
+		:author => "Eric Evans"
+	}
+}
+
+puts books[:gourd_of_flies][:author]
+
+puts books[:enterprise_app_architecture][:name]
