df606fde26d6ab955608dc291d39d304a06f31ae
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-07 17:59:15 -0500

    Add second test for #get_paid

--- ruby/writing_rspec_tests/spec/person_spec.rb
+++ ruby/writing_rspec_tests/spec/person_spec.rb
@@ -8,4 +8,13 @@ describe Person do
 			expect(p.money).to eq(500)
 		end
 	end
+
+	describe "#get_paid" do
+		it "adds the salary to the total money" do
+			p = Person.new(500)
+			p.get_paid(300)
+
+			expect(p.money).to eq(800)
+		end
+	end
 end
