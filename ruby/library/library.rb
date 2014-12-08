531c3e02ecc6afb3800484677abf925d7fd0acd9
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-07 21:32:30 -0500

    Add code if statement to avoid book to be checked out twice. 4 examples, 0 failures.

--- ruby/library/library.rb
+++ ruby/library/library.rb
@@ -8,4 +8,14 @@ class Book
 		@author = author
 		@status = status
 	end
+
+	def check_out
+		if @status == 'available'
+			@status = 'checked_out'
+			true
+		else
+			puts "The book has been checked_out already."
+			false
+		end
+	end
 end
