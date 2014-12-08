840c749ff06b65e2c57ab220c4ca17929d1f7942
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-07 20:10:22 -0500

    Add status to book with a default status of available and pass 2nd test

--- ruby/library/library.rb
+++ ruby/library/library.rb
@@ -1,10 +1,11 @@
 class Book
 
-	attr_accessor :title, :author
+	attr_accessor :title, :author, :status
 	attr_reader :id
 
-	def initialize(title, author, id=nil)
+	def initialize(title, author, status='available', id=nil)
 		@title = title
 		@author = author
+		@status = status
 	end
 end
