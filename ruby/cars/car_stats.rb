60226062bf1ce472ae33c679b052d37737f90050
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 13:58:18 -0500

    Create method self.calc_top_color that returns the most popular car color

--- ruby/cars/car_stats.rb
+++ ruby/cars/car_stats.rb
@@ -1,3 +1,29 @@
 class CarStats
-  # TODO
+
+  def self.calc_top_color(cars)
+  	top = Hash.new (0)
+  	cars.each { |car| top[car.color] += 1 }
+  	top.sort_by { |color, count| count }.last.first
+	end
+
+
+
+
+  ######################
+  # POP & SHIFT OPTION #
+  ######################
+
+  # def self.calc_top_color(cars)
+  #   car_colors = car_colors_array(cars)
+  #   car_colors.sort_by {|color| car_colors.count(color)}.pop
+  # end
+
+  # def self.car_colors_array(cars)
+  #   cars.map {|car| car.color}
+  # end
+
+  # def self.calc_bottom_color(cars)
+  #   car_colors = car_colors_array(cars)
+  #   car_colors.sort_by {|color| car_colors.count(color)}.shift
+  # end
 end
