903c44cb20fe472a81de864d967dd23b1c1204ec
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 12:36:43 -0500

    Add test to raise an error if car is created without color and another that verifies that car honk

--- ruby/cars/spec/car_spec.rb
+++ ruby/cars/spec/car_spec.rb
@@ -8,4 +8,12 @@ describe 'Car' do
 	it 'initializes with a color' do
 		expect(@car.color).to eq('black')
 	end
+
+	it 'throws an error if a color is not provided on initialize' do
+		expect { Car.new }. to raise_error
+	end
+
+	it 'honks' do
+		expect(@car.honk).to eq('meep')
+	end 
 end
