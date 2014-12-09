bb08320b1d8e1296e5413c6da0d42c376596c194
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 14:44:03 -0500

    Delete unneccessary code. 3 possible ways to find the least and most popular car color.

--- ruby/cars/car_stats.rb
+++ ruby/cars/car_stats.rb
@@ -24,37 +24,35 @@ class CarStats
   # POP & SHIFT #
   ###############
 
-  def self.most_popular_color(cars)
-    car_colors = cars.map {|car| car.color}
-    car_colors.sort_by {|color| car_colors.count(color)}.pop
-  end
+  # def self.most_popular_color(cars)
+  #   car_colors = cars.map {|car| car.color}
+  #   car_colors.sort_by {|color| car_colors.count(color)}.pop
+  # end
 
-  def self.least_popular_color(cars)
-    car_colors = cars.map {|car| car.color}
-    car_colors.sort_by {|color| car_colors.count(color)}.shift
-  end
+  # def self.least_popular_color(cars)
+  #   car_colors = cars.map {|car| car.color}
+  #   car_colors.sort_by {|color| car_colors.count(color)}.shift
+  # end
 
 
   #############
   # MAX & MIN #
   #############
 
-  # attr_accessor :cars, :pop_color
+  def self.all_colors(cars)
+  	@count = cars.inject(Hash.new(0)) do |color, car| 
+      color[car.color] += 1
+      color
+    end
+  end
 
-  # def self.all_colors(cars)
-  # 	@count = cars.inject(Hash.new(0)) do |color, car| 
-  #     color[car.color] += 1
-  #     color
-  #   end
-  # end
+  def self.most_popular_color(cars)
+    self.all_colors(cars)
+    @count.max_by {|k, v| v}.first
+  end
 
-  # def self.most_popular_color(cars)
-  #   self.all_colors(cars)
-  #   @count.max_by {|k, v| v}.first
-  # end
-
-  # def self.least_popular_color(cars)
-  # 	self.all_colors(cars)
-  #   @count.min_by {|k, v| v}.first
-  # end
+  def self.least_popular_color(cars)
+  	self.all_colors(cars)
+    @count.min_by {|k, v| v}.first
+  end
 end
