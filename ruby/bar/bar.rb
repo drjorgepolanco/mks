fc0472f5fe90c81bb6a78c1d4bfff9faf1a5b32d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 15:38:09 -0500

    Tests: initializes with a name and cannot change its name. Both passing.

--- ruby/bar/bar.rb
+++ ruby/bar/bar.rb
@@ -1,4 +1,9 @@
 require 'time'
 
 class Bar
+	attr_reader :name
+
+	def initialize(name)
+		@name = 'The Irish Yodel'
+	end
 end
--- ruby/bar/spec/bar_spec.rb
+++ ruby/bar/spec/bar_spec.rb
@@ -7,4 +7,8 @@ describe Bar do
 	it 'initializes with a name' do
 		expect(@bar.name).to eq('The Irish Yodel')
 	end
+
+	it 'cannot change its name' do
+		expect { @bar.name = 'lolcat cave' }.to raise_error
+	end
 end
