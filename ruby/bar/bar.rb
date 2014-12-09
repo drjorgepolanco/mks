24b0f2492191c03b445eb0b9e0cdb3d7759140db
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 15:50:23 -0500

    Add method add_menu_item. All tests passing.

--- ruby/bar/bar.rb
+++ ruby/bar/bar.rb
@@ -4,8 +4,12 @@ class Bar
 	attr_reader :name
 	attr_accessor :menu_items
 
-	def initialize(name, menu_items = [])
+	def initialize(name, menu_items = {})
 		@name = 'The Irish Yodel'
 		@menu_items = menu_items
+	end
+
+	def add_menu_item(item, price)
+		@menu_items[item] = price
 	end
 end
