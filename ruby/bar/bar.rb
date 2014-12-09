f63ac9d8110362b585c879681defef96b36997f7
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 15:41:36 -0500

    Add menu_items as an empty string

--- ruby/bar/bar.rb
+++ ruby/bar/bar.rb
@@ -2,8 +2,10 @@ require 'time'
 
 class Bar
 	attr_reader :name
+	attr_accessor :menu_items
 
-	def initialize(name)
+	def initialize(name, menu_items = [])
 		@name = 'The Irish Yodel'
+		@menu_items = menu_items
 	end
 end
