f7a2038f170ba9de88218ebbe56affe31c67e6b3
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 16:05:14 -0500

    Add class Item and initialize, change @menu_items to become an array instead of a hash

--- ruby/bar/bar.rb
+++ ruby/bar/bar.rb
@@ -1,15 +1,24 @@
 require 'time'
 
 class Bar
-	attr_reader :name
-	attr_accessor :menu_items
+	attr_reader :name, :menu_items
 
-	def initialize(name, menu_items = {})
+	def initialize(name, menu_items = [])
 		@name = 'The Irish Yodel'
 		@menu_items = menu_items
 	end
 
-	def add_menu_item(item, price)
-		@menu_items[item] = price
+	def add_menu_item(name, price)
+		item = Item.new(name, price)
+		@menu_items.push(item)
+	end
+end
+
+class Item
+	attr_accessor :name, :price
+
+	def initialize(name, price)
+		@name = name
+		@price = price
 	end
 end
