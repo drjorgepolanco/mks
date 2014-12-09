78d1773902caebd4ac8d2d35ebc90b630a523630
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 14:42:32 -0500

    Finalized excersive with 3 different ways to find the most and least popular car, DRY

--- ruby/cars/car_stats.rb
+++ ruby/cars/car_stats.rb
@@ -1,33 +1,60 @@
 class CarStats
 
+	################
+  # FIRST & LAST #
+  ################
+
+	# def self.all_colors(cars)
+	# 	@popular_cars = Hash.new(0)
+	# 	cars.each { |car| @popular_cars[car.color] += 1 }
+	# end
+
+ 	# def self.most_popular_color(cars)
+ 	#  	self.all_colors(cars)
+ 	#  	@popular_cars.sort_by { |color, count| count }.last.first
+	# end
+
+	# def self.least_popular_color(cars)
+	# 	self.all_colors(cars)
+	# 	@popular_cars.sort_by { |color, count| count }.first.first
+	# end
+
+
+  ###############
+  # POP & SHIFT #
+  ###############
+
   def self.most_popular_color(cars)
-  	most_popular = Hash.new(0)
-  	cars.each { |car| most_popular[car.color] += 1 }
-  	most_popular.sort_by { |color, count| count }.last.first
-	end
+    car_colors = cars.map {|car| car.color}
+    car_colors.sort_by {|color| car_colors.count(color)}.pop
+  end
 
-	def self.least_popular_color(cars)
-		least_popular = Hash.new(0)
-		cars.each {|car| least_popular[car.color] += 1 }
-		least_popular.sort_by { |color, count| count }.first.first
-	end
+  def self.least_popular_color(cars)
+    car_colors = cars.map {|car| car.color}
+    car_colors.sort_by {|color| car_colors.count(color)}.shift
+  end
 
 
-  ######################
-  # POP & SHIFT OPTION #
-  ######################
+  #############
+  # MAX & MIN #
+  #############
 
-  # def self.calc_top_color(cars)
-  #   car_colors = car_colors_array(cars)
-  #   car_colors.sort_by {|color| car_colors.count(color)}.pop
+  # attr_accessor :cars, :pop_color
+
+  # def self.all_colors(cars)
+  # 	@count = cars.inject(Hash.new(0)) do |color, car| 
+  #     color[car.color] += 1
+  #     color
+  #   end
   # end
 
-  # def self.car_colors_array(cars)
-  #   cars.map {|car| car.color}
+  # def self.most_popular_color(cars)
+  #   self.all_colors(cars)
+  #   @count.max_by {|k, v| v}.first
   # end
 
-  # def self.calc_bottom_color(cars)
-  #   car_colors = car_colors_array(cars)
-  #   car_colors.sort_by {|color| car_colors.count(color)}.shift
+  # def self.least_popular_color(cars)
+  # 	self.all_colors(cars)
+  #   @count.min_by {|k, v| v}.first
   # end
 end
