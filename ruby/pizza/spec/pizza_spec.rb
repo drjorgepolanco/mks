9ac1297ced5c6a0951af331a1c491c7648acd1c5
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-08 19:39:36 -0500

    Create pizza project. Introduction to RSPEC. Create first test: initialize(name). Initial commit.

---
+++ ruby/pizza/spec/pizza_spec.rb
@@ -0,0 +1,21 @@
+require './pizza'
+
+describe Pizza do
+  it "exists" do
+    expect(Pizza).to be_a(Class)
+  end
+end
+
+describe Topping do
+  it "exists" do
+    expect(Topping).to be_a(Class)
+  end
+
+  describe '.initialize' do
+  	it "sets the name of the Topping" do
+  		topping = Topping.new('olives')
+
+  		expect(topping.name).to eq('olives')
+  	end
+  end
+end
