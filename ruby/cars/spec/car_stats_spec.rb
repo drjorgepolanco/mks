a15bcbe02e0bb5221359757b554d35850fa2460d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-09 13:49:02 -0500

    Fix bug, Cars.new to Car.new and silver color to red

--- ruby/cars/spec/car_stats_spec.rb
+++ ruby/cars/spec/car_stats_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe 'CarStats' do
 	before do
 		@cars = []
-		3.times { @cars << Cars.new('black') }
-		5.times { @cars << Cars.new('red') }
-		2.times { @cars << Cars.new('white') }
+		3.times { @cars << Car.new('black') }
+		5.times { @cars << Car.new('red') }
+		2.times { @cars << Car.new('white') }
 	end
 
 	it 'calculates the most popular car color' do
 		pop_color = CarStats.calc_top_color(@cars)
-		expect(pop_color).to eq('silver')
+		expect(pop_color).to eq('red')
 	end
 end
