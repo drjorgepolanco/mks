8d5147b8ef8ad1673106032437e382db8d6aaf24
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 11:24:40 -0500

    Ruby Review. Behavior Driven Development. Initial commit

---
+++ ruby/review/review.rb
@@ -0,0 +1,120 @@
+# # # # # # #
+# Problem 1 #
+# # # # # # #
+
+module GettersSetters
+
+  # # # # # #
+  # Setters #
+  # # # # # #
+
+  class Doctor
+    attr_reader :skill
+
+    def initialize
+      @skill = "bedside manners"
+    end
+    # TODO: Write a setter method for @skill
+  end
+
+end
+
+
+class HashProblems
+  # # # # # # #
+  # Hashes  1 #
+  # # # # # # #
+
+  def self.update_top_compartment_item(wardrobe_hash)
+    # TODO: Complete this method by writing A SINGLE LINE
+  end
+
+end
+
+
+class ArrayProblems
+
+  # # # # # # #
+  # Arrays  1 #
+  # # # # # # #
+
+  def self.add_cat_to_array(array)
+    # TODO: Complete this method
+  end
+
+  # # # # # # #
+  # Arrays  2 #
+  # # # # # # #
+
+  def self.list_wardrobe_item_sizes(array)
+    # TODO: Use the map method to create an array of wardrobe item sizes
+  end
+
+  # # # # # # #
+  # Arrays  3 #
+  # # # # # # #
+
+  def self.tell_me_the_weather(array)
+    # TODO: Use the map method to create an array of weather descriptions
+  end
+
+  # # # # # # #
+  # Arrays  4 #
+  # # # # # # #
+
+  def self.list_my_hats(array)
+    # TODO: Use the map method to create an array of sized hats
+  end
+end
+
+
+class MethodReturns
+
+  def self.include?(array, search_item)
+    # TODO: Make this method return true or false depending whether
+    #       or not the search_item exists in the array.
+    array.each do |elem|
+      if elem == search_item
+        true
+      end
+    end
+    false
+  end
+
+  def self.get_name
+    puts "Bob"
+  end
+
+end
+
+
+module Scopes
+
+  class Person
+    def jump(height)
+      jump_height = height
+      "I can jump #{jump_height} inches!"
+    end
+
+    def last_jump_height
+      "I last jumped #{jump_height} inches."
+    end
+  end
+
+  class Finder
+    def initialize(people)
+      @people = people
+    end
+
+    def find_first(salary)
+      @people.each do |person|
+        if person[:salary] == salary
+          found = person
+        end
+      end
+
+      found
+    end
+  end
+
+end
---
+++ ruby/review/spec/review_spec.rb
@@ -0,0 +1,130 @@
+require 'spec_helper'
+
+describe "Ruby Review: You" do
+	describe "your knowledge of getters and setters" do
+		you "know how to write a setter method" do
+			doctor = GettersSetters::Doctor.new
+			doctor.skill = 'patient management'
+			expect(doctor.skil).to eq('patient management')
+
+			expect(@source).to_not include_code(:attr_writer).in(:Doctor).for(:GettersSetters)
+		end
+	end
+
+	describe "Your knowledge of Hashes" do
+		xyou "know how to change a value in a hash" do
+			wardrobe = {
+				"top compartment" => {
+					:item => "neck tie"
+				},
+				"lower compartment" => "pants"
+			}
+
+			HashProblems.update_top_compartment_item(wardrobe)
+			compartment = wardrobe["top compartment"]
+			expect(compartment[:item]).to eq("fake beard")
+
+			expect(@source).to include_class_method(:HashProblems, :update_top_compartment_item).with_line_count(1)
+		end
+	end
+
+	describe "Your knowledge of Arrays" do
+		xyou "know how to add a hash to an array" do
+			pets = [{:dog => 'Lucky'}, {:parrot => 'Polly'}]
+			ArrayProblems.add_cat_to_array(pets)
+			expect(pets).to eq([{:dog => 'Lucky'}, {:parrot => 'Polly'}, {:cat => 'Pogo'}])
+
+			pets = [{:chipmunk => 'Joel'}, {:snake => 'Monty'}]
+
+			ArrayProblems.add_cat_to_array(pets)
+			expect(pets).to eq([{ :chipmunk => 'Joel'}, { :snake => 'Monty'}, { :cat => 'Pogo'}])
+		end
+
+		xyou "know how to use the map method part 1" do
+			wardrobe_items = [
+				{:name => 'coat', :size => 'medium'},
+				{:name => 'hat', :size => 'large'}
+			]
+
+			result = ArrayProblems.list_wardrobe_items_sizes(wardrobe_items)
+			expect(result).to eq(['medium', 'large'])
+
+			expect(@source).to include_code(:map).in_class_method(:ArrayProblems, :list_wardrobe_items_sizes)
+		end
+
+		xyou "know how to use map method part 2" do
+			weather = ['drizzly', 'cold']
+
+			result = ArrayProblems.tell_me_the_weather(weather)
+			expect(result).to eq(['It is drizzly', 'It is cold'])
+
+			expect(@souce).to include_code(:map).in_class_method(:ArrayProblems, :tell_me_the_weather)
+		end
+
+		xyou "know how to use the map method part 3" do
+			hat_collection = [
+				{:size => 'medium', :style => 'cap'},
+				{:size => 'small', :style => 'fedora'}
+			]
+
+			result = ArrayProblems.list_my_hats(hat_collection)
+			expect(result).to eq(['medium cap', 'small fedora'])
+
+			expect(@source).to include_code(:map).in_class_method(:ArrayProblems, :list_my_hats)
+		end
+	end
+
+	describe "your knowledge of implicit returns" do
+		xyou "know how to explicitly return a statement" do
+			numbers_array = [4, 5, 2]
+			result = MethodReturns.include?(numbers_array, 2)
+			expect(result).to eq(true)
+
+			result = MethodReturns.include?(numbers_array, 4)
+			expect(result).to eq(true)
+
+			result = MethodReturns.include?(numbers_array, 7)
+			expect(result).to eq(false)
+		end
+
+		xyou "know what puts returns" do
+			result = MethodReturns.get_name
+			expect(result).to eq('Bob')
+		end
+	end
+
+	describe "your knowledge of scope in Ruby" do
+		xyou "know about method scope" do
+			bob = Scopes::Person.new
+			expect(bob.jump(10)).to eq('I can jump 10 inches!')
+			expect(bob.last_jump_height).to eq('I last jumped 10 inches.')
+
+			expect(bob.jump(2)).to eq("I can jump 2 inches!")
+      expect(bob.last_jump_height).to eq("I last jumped 2 inches.")
+		end
+
+		xyou "know about block scope" do
+			data = [
+				{:name => "Joe", :salary => 100},
+        {:name => "Sally", :salary => 1000}
+			]
+
+			finder = Scopes::Finder.new(data)
+
+			person = finder.find_first(1000)
+			expect(person[:name]).to eq('Sally')
+
+			person = finder.find_first(100)
+			expect(person[:name]).to eq('Joe')
+
+			person = finder.find_first(999)
+			expect(person[:name]).to be_nil
+
+			expect(@source).to_not include_code(:return).in(:Finder).for(:Scopes)
+		end
+	end
+
+	before(:all) do
+		@source = RubyParser.new.parse(File.read('review.rb'), './review.rb')
+	end
+end
