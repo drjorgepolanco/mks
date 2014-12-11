7b3102d1b3c298836b11b8ff1bd82d25fe1ea0cc
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 17:53:31 -0500

    automatically gets an id upon creation

--- ruby/task-manager/spec/project_spec.rb
+++ ruby/task-manager/spec/project_spec.rb
@@ -9,4 +9,8 @@ describe 'Project' do
   it "is created with a name" do
   	expect(project.name).to eq("Project One")
   end
+
+  it "automatically gets an id upon creation" do
+  	expect(project.id).to be_a(Fixnum)
+  end
 end
