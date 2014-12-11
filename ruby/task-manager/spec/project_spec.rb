f4694398f59e060e4e81817dab61960ae73bfe04
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 18:48:47 -0500

    it's created with a name

--- ruby/task-manager/spec/project_spec.rb
+++ ruby/task-manager/spec/project_spec.rb
@@ -6,4 +6,18 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "is created with a name" do
+    expect(project.name).to eq("Project One")
+  end
+
+  xit "automatically gets an id upon creation" do
+    expect(project.id).to be_a(Fixnum)
+  end
+
+  xit "can add tasks to an existing project" do
+    task = TM::Task.new("Do first thing", 'high', 1)
+    project.new_task(task)
+    expect(project.tasks.first).to eq(task)
+  end
 end
