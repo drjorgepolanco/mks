434ca4de76dbefc4da06f66c4ae25fdb3b2e54f9
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 18:33:37 -0500

    Initialize task

--- ruby/task-manager/lib/task-manager/task.rb
+++ ruby/task-manager/lib/task-manager/task.rb
@@ -1,2 +1,10 @@
 class TM::Task
+	attr_accessor :description, :priority
+	attr_reader :project_id
+
+	def initialize(description, priority, project_id)
+		@description = description
+		@priority = priority
+		@project_id = project_id
+	end
 end
