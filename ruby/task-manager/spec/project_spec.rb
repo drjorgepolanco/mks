4d7c418040e380ffde8c62a05a3ab06f2f69924f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 18:48:01 -0500

    Delete absolutely everything. Start from 0

--- ruby/task-manager/spec/project_spec.rb
+++ ruby/task-manager/spec/project_spec.rb
@@ -6,18 +6,4 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
-
-  it "is created with a name" do
-  	expect(project.name).to eq("Project One")
-  end
-
-  it "automatically gets an id upon creation" do
-  	expect(project.id).to be_a(Fixnum)
-  end
-
-  it "can add tasks to an existing project" do
-  	task = TM::Task.new("Do first thing", 'high', 1)
-  	project.new_task(task)
-  	expect(project.tasks.first).to eq(task)
-  end
 end
