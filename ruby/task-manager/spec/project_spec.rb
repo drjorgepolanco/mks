f867b6cc0a222ef196392dcdb57e27351c32bbde
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 18:22:45 -0500

    It can add tasks to a project

--- ruby/task-manager/spec/project_spec.rb
+++ ruby/task-manager/spec/project_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'Project' do
 	let(:project) { TM::Project.new("Project One")}
+  
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
@@ -12,5 +13,11 @@ describe 'Project' do
 
   it "automatically gets an id upon creation" do
   	expect(project.id).to be_a(Fixnum)
+  end
+
+  it "can add tasks to an existing project" do
+  	task = TM::Task.new("Do first thing", 'high', 1)
+  	project.new_task(task)
+  	expect(project.tasks.first).to eq(task)
   end
 end
