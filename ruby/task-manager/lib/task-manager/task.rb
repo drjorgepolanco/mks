6ae4b42834c0edbdaf1ffe4c9db997db7194df17
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 21:29:25 -0500

    It has it's own id. green.

--- ruby/task-manager/lib/task-manager/task.rb
+++ ruby/task-manager/lib/task-manager/task.rb
@@ -1,10 +1,15 @@
 class TM::Task
-	attr_reader :description, :project_id
-	attr_accessor :priority
+	attr_reader :description, :project_id, :id
+	attr_accessor :priority, :status
 
-	def initialize(description, priority=1, project_id=1)
+	@@task_counter = 0
+	
+	def initialize(description, priority=1, status='incomplete', project_id=1, id=1)
 		@description = description
 		@project_id = project_id
 		@priority = priority
+		@status = status
+		@@task_counter += 1
+		@id = @@task_counter
 	end
 end
