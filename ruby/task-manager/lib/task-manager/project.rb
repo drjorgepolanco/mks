17f55d994638ac3c07cf1a042e48fb21e1430e96
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 19:40:07 -0500

    Add task_list array, accessor method for task_list and add_task method. Test green.

--- ruby/task-manager/lib/task-manager/project.rb
+++ ruby/task-manager/lib/task-manager/project.rb
@@ -1,5 +1,6 @@
 class TM::Project
 	attr_reader :name, :id
+	attr_accessor :task_list
 
 	@@counter = 0
 
@@ -7,5 +8,12 @@ class TM::Project
 		@name = name
 		@@counter += 1
 		@id = @@counter
+		@task_list = []
+	end
+
+	def add_task(description)
+		project_id = @id
+		new_task = TM::Task.new(description, project_id)
+		@task_list << new_task
 	end
 end
