22d34ac288289deb9756e5f089629581f070df3e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 19:42:50 -0500

    It starts with a description

--- ruby/task-manager/spec/task_spec.rb
+++ ruby/task-manager/spec/task_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe 'Task' do
+	let(:task) { TM::Task.new("Task One")}
+
   it "exists" do
     expect(TM::Task).to be_a(Class)
+  end
+
+  it "starts with a description" do
+  	expect(task.description).to eq("Task One")
   end
 end
