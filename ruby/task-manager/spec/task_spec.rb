ec366b89ab5849fac151027f53964f22927b2f9e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-11 19:46:29 -0500

    The task always belongs to a project

--- ruby/task-manager/spec/task_spec.rb
+++ ruby/task-manager/spec/task_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe 'Task' do
-	let(:task) { TM::Task.new("Task One")}
+	let(:project) { TM::Project.new("The Project Name") }
+	let(:task) { TM::Task.new("Task One") }
 
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+  describe '#initialize' do
+	  it "starts with a description" do
+	  	expect(task.description).to eq("Task One")
+	  end
 
-  it "starts with a description" do
-  	expect(task.description).to eq("Task One")
-  end
+	  it "belongs to a project" do
+	  	project.add_task(task)
+	  	expect(task.project_id).to eq(1)
+	  end
+	end
 end
