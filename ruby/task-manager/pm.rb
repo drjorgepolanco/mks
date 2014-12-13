7fd33d4c7facdd8c7215dc4e4ee8449ee09e4a4e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-12 23:55:59 -0500

    Add basic intro message for user interface

--- ruby/task-manager/pm.rb
+++ ruby/task-manager/pm.rb
@@ -51,3 +51,20 @@ class TM::Manager
 		project.tasks
 	end
 end
+
+
+manager = TM::Manager.new
+
+puts "Welcome to Task Manager Pro® v1.0"
+puts "How can I help you?"
+
+puts "Use the following commands to interact with the Task Manager:"
+
+puts "HELP 				- Show these commands again."
+puts "ALL     		- Displays a list of all the projects."
+puts "NEW 				- To create new projects."
+puts "ADD      		- To add new tasks to an existing project."
+puts "DONE 				- To mark a task as 'complete'."
+puts "COMPLETE 		- To list all completed tasks of an existing project."
+puts "INCOMPLETE 	-	To list all incompleted tasks of an existing project."
+
