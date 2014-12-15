6604cf2ca1741532c5af906f8f642e992d5de92c
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-14 20:17:35 -0500

    Generate:init, Create table Users and run migration = Schema.

--- ruby/active_record/config/environments.rb
+++ ruby/active_record/config/environments.rb
@@ -3,6 +3,6 @@ require 'active_record_tasks'
 require_relative '../lib/ar_exercise.rb'
 
 ActiveRecord::Base.establish_connection(
-	:adapter => 'postgresql',
-	:database => 'ar_exercise'
+  :adapter => 'postgresql',
+  :database => 'ar_exercise'
 )
