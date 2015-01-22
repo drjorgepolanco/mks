512f3a8b79986437314b59b001af7c6be0ae1f80
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-22 14:45:46 -0500

    Loop through items array and display all items dynamically

--- immersive/javascript/riot_js/riot-templates/js/main.js
+++ immersive/javascript/riot_js/riot-templates/js/main.js
@@ -1,3 +1,18 @@
 var itemTemplate = $('#templates .item').html();
-var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', item: 0.15 });
-$('#store').append(newItemHtml);
+// var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+// $('#store').append(newItemHtml);
+
+var items = [
+  { name: "blueberries", price: 1.15 },
+  { name: "bananas", price: 0.26 },
+  { name: "avocados", price: 0.89 }
+];
+
+for (var i = 0; i < items.length; i += 1) {
+  var newItems = $.render(itemTemplate, { 
+  	name: items[i].name, 
+  	price: items[i].price 
+  });
+  $('#store').append(newItems);
+};
+
