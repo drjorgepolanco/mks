a9f230e0ff5c3d7b2cb22219fba1c8ef7b788a74
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-22 14:50:29 -0500

    Add commented intructions and improve code

--- immersive/javascript/riot_js/riot-templates/js/main.js
+++ immersive/javascript/riot_js/riot-templates/js/main.js
@@ -1,12 +1,28 @@
 var itemTemplate = $('#templates .item').html();
-// var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
-// $('#store').append(newItemHtml);
+var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+$('#store').append(newItemHtml);
+
+
+// -----------------------------------------------------------------------------
+// Parnert Exercise #1
+// -------------------
+
+// Create an array of object literals.
 
 var items = [
   { name: "blueberries", price: 1.15 },
   { name: "bananas", price: 0.26 },
   { name: "avocados", price: 0.89 }
 ];
+
+
+// -----------------------------------------------------------------------------
+// Parnert Exercise #2
+// -------------------
+
+// Write code to loop through this array. On each iteration:
+// - Use your template to generate HTML for the current object.
+// - Add this new HTML to the page.
 
 for (var i = 0; i < items.length; i += 1) {
   var newItems = $.render(itemTemplate, { 
