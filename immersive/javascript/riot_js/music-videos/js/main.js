709daab40bdb641064e1d4107405792f54b2df3f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-22 22:27:36 -0500

    Complete third step: 'Creating the Form'. Music Videos v1.0 complete.

--- immersive/javascript/riot_js/music-videos/js/main.js
+++ immersive/javascript/riot_js/music-videos/js/main.js
@@ -1,15 +1,29 @@
 var videos = [
-  { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM' },
-  { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
+  { title: "Get Krazy at Chuck E Cheese's", youtubeId: 'GyR2HJ9B7aM' },
+  { title: "Spartans Kill Innocent Persians", youtubeId: 'sIeJSSjTG3k' }
 ];
 
 var videoTemplate = $('#templates .video-list-item').html();
 
 var renderVideoList = function () {
   for (var i = 0; i < videos.length; i += 1) {
-    var video = $.render(videoTemplate, { title: videos[i].title });
+    var video = $.render(videoTemplate, { 
+    	title: videos[i].title, 
+    	youtubeId: videos[i].youtubeId 
+    });
     $('#video-list').append(video);
   };
 };
 
 renderVideoList();
+
+$('#new-video').on('submit', function (e) {
+	e.preventDefault();
+	var title = $('.title').val();
+	var youtubeId = $('.youtube_id').val();
+	var newVideo = $.render(videoTemplate, { 
+		title: title, 
+		youtubeId: youtubeId
+	});
+	$('#video-list').append(newVideo);
+});
