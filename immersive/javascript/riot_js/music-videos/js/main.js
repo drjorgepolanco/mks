31678aafe3e3a4bdaf5ccb9d47ea00905ba70a87
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-23 21:28:26 -0500

    Add extra sample videos with genres and add opacity to background image

--- immersive/javascript/riot_js/music-videos/js/main.js
+++ immersive/javascript/riot_js/music-videos/js/main.js
@@ -1,6 +1,13 @@
 var videos = [
-  { title: "Get Krazy at Chuck E Cheese's", youtubeId: 'GyR2HJ9B7aM' },
-  { title: "Spartans Kill Innocent Persians", youtubeId: 'sIeJSSjTG3k' }
+  { title: "Iron Maiden Wasted Years Official Music Video",           youtubeId: 'GnqkDbrIfps', genre: 'metal'        },
+  { title: "Blake Shelton - 'Lonely Tonight' (Ft. Ashley Monroe)",    youtubeId: 'G91KZ56mNbw', genre: 'country'      },
+  { title: "SODA STEREO - De musica ligera en vivo",                  youtubeId: 'PBozofUgEQ0', genre: 'spanish rock' },
+  { title: "B. B. King - The Thrill Is Gone (Live at Montreux 1993)", youtubeId: '4fk2prKnYnI', genre: 'blues'        },
+  { title: "Michel Camilo - New York Band - 'CARIBE' (Solo Intro)",   youtubeId: 'FC9MKexsNnU', genre: 'jazz'         },
+  { title: "Enanitos Verdes - Lamento Boliviano (video oficial)",     youtubeId: 'khbDnLqe_Wk', genre: 'spanish rock' },
+  { title: "Chick Corea Elektric Band Live at North Sea Jazz 2003",   youtubeId: 'SL34LYIWQ6M', genre: 'jazz'         },
+  { title: "Art Tatum Solo Masterpieces - Volume 1",                  youtubeId: '1SnJSHfMAxQ', genre: 'jazz'         },
+  { title: "Metallica - Nothing Else Matters [Official Music Video]", youtubeId: 'tAGnKpE4NCI', genre: 'metal'        }
 ];
 
 var videoTemplate = $('#templates .video-list-item').html();
@@ -14,7 +21,6 @@ var renderVideoList = function () {
     $('#video-list').append(video);
   };
 };
-
 renderVideoList();
 
 $('#new-video').on('submit', function (e) {
