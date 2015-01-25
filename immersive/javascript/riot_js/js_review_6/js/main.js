80ee18f9ddef1431ca4444b445aa225b627982f2
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-25 17:32:09 -0500

    JavaScript Review 6: $.render and $.observable. Finish Review Exercises

--- immersive/javascript/riot_js/js_review_6/js/main.js
+++ immersive/javascript/riot_js/js_review_6/js/main.js
@@ -181,17 +181,133 @@ result += last + "!";
 // result += array.join(', ') + "!";
 
 console.log('Result should be "bop it, pull it, twist it!":', result);
+// → Result should be "bop it, pull it, twist it!": bop it, pull it, twist it!
 
 
+// -----------------------------------------------------------------------------
+// Partner Exercise #4.1
+// ---------------------
+
+// Complete the object so that the string renders correctly:
+
+var greetingHtml = $.render('<p>Welcome, { username }</p>', { 
+	username: 'xXxDarkxAssassinxXx' 
+});
+
+console.log('Result should be <p>Welcome, xXxDarkxAssassinxXx</p>', greetingHtml);
+// → Result should be <p>Welcome, xXxDarkxAssassinxXx</p> 
+// →                  <p>Welcome, xXxDarkxAssassinxXx</p>
 
 
+// -----------------------------------------------------------------------------
+// Partner Exercise #4.2
+// ---------------------
+
+// Complete the string so that the console.log outputs the correct value:
+
+var giantButtonHtml = $.render('<a class="giant button { className }">Color is not { color }</a>', {
+	color: 'pickle-green', 
+	className: 'not-green' 
+});
+
+console.log('Result should be <a class="giant button not-green">Color is not pickle-green</a>', 
+	giantButtonHtml);
+// → Result should be <a class="giant button not-green">Color is not pickle-green</a> 
+// →                  <a class="giant button not-green">Color is not pickle-green</a>
 
 
+// -----------------------------------------------------------------------------
+// Partner Exercise #4.3
+// ---------------------
+
+// Create the template string so that the console.log renders correctly:
+
+var giantBoxTemplate = '<div id="{ id }" class="giant">{ content }</div>';
+
+var giantBoxHtml = $.render(giantBoxTemplate, { id: 'loud', content: 'AHOY!' });
+console.log('Result should be <div id="loud" class="giant">AHOY!</div>', giantBoxHtml)
+// → Result should be <div id="loud" class="giant">AHOY!</div> 
+// →                  <div id="loud" class="giant">AHOY!</div>
 
 
+// -----------------------------------------------------------------------------
+// Partner Exercise #4.4
+// ---------------------
+
+// Create the template script on the html page so that the console.log renders correctly. 
+
+var tastyItemTemplate = $('#templates .yummy').html();
+var tastyItemHtml = $.render(tastyItemTemplate, { 
+	item: 'Creme Puff', 
+	tastiness: 'Oh so good', 
+	className: 'yummy' 
+});
+console.log('Result should be <div class="yummy box">The tasty thing is: Creme Puff. Its rating is: Oh so good</div>', tastyItemHtml);
+// → Result should be <div class="yummy box">The tasty thing is: Creme Puff. Its rating is: Oh so good</div> 
+// →                  <div class="yummy box">The tasty thing is: Creme Puff. Its rating is: Oh so good</div>
 
 
+// -----------------------------------------------------------------------------
+// Partner Exercise #5.1
+// ---------------------
+
+// Write an additional line of code to trigger only the 'turkey-run' event:
+
+var poultryRace = $.observable({});
+
+poultryRace.on('turkey-run', function () {
+	console.log('gobble! gobble!');
+});
+
+poultryRace.on('chicken-waddle', function () {
+	console.log('kachirp! kachirp!');
+});
+
+poultryRace.trigger('turkey-run');
+// → gobble! gobble!
 
 
+// -----------------------------------------------------------------------------
+// Partner Exercise #5.2
+// ---------------------
 
+// Finish the line of code so that triggering 10-story-jump on stuntMan alerts 
+// the message I beLIEVE!:
+
+var stuntMan = $.observable({});
+
+stuntMan.on('10-story-jump', function () {
+	console.log('I beLIEVE!');
+});
+
+stuntMan.trigger('10-story-jump');
+// → I beLIEVE!
+
+
+// -----------------------------------------------------------------------------
+// Partner Exercise #5.3
+// ---------------------
+
+// Write the missing line of code so that the following code makes it rain:
+
+var makeItRain = {
+	rainContent: 'dollar bills, yo'
+};
+
+console.log('Before:', makeItRain);
+// → Before: Object {rainContent: "dollar bills, yo"}
+
+$.observable(makeItRain);
+
+console.log('After:', makeItRain);
+// → After: Object {rainContent: "dollar bills, yo", on: function, off: function, 
+//   one: function, trigger: function}
+
+
+makeItRain.on('rain', function () {
+	console.log("It's raining " + makeItRain.rainContent + '.');
+});
+
+makeItRain.trigger('rain');
+// → It's raining dollar bills, yo.
 
