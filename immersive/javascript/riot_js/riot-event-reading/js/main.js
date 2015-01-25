7323b9b39808cefa03fb0efbc62ab33618a73a28
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-24 20:08:03 -0500

    Finish Riot Event Reading Exercise. Update gitignore of riot-events to avoid .DS_Store

---
+++ immersive/javascript/riot_js/riot-event-reading/js/main.js
@@ -0,0 +1,15 @@
+(function () {
+
+	clock.on('tick', function (tickCount) {
+		console.log('tick!', tickCount);
+	});
+
+	$('#clock-control .start').on('click', function (e) {
+		clock.start();
+	});
+
+	$('#clock-control .stop').on('click', function (e) {
+		clock.stop();
+	});
+
+})();
