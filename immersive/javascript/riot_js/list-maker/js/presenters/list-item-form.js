dbf9c0afb7f7fd0f86115d584985010f6e600bfa
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-29 16:33:12 -0500

    Test commit. Indentation issues

--- immersive/javascript/riot_js/list-maker/js/presenters/list-item-form.js
+++ immersive/javascript/riot_js/list-maker/js/presenters/list-item-form.js
@@ -1,35 +1,35 @@
 (function () {
 
 	var $view = $('form.new-list-item'), 
-	            invalidText = '(!) You must include both an item name and an item category.';
+							invalidText = '(!) You must include both an item name and an item category.';
 
 
 // HTML interactions
 
-  $view.on('submit', function (e) {
+	$view.on('submit', function (e) {
 
-  	e.preventDefault();
+		e.preventDefault();
 
-    // Grab user input data
-  	newListItem = {
-  		name: $('.name', $view).val(),
-  		category: $('.category', $view).val()
-  	};
+		// Grab user input data
+		newListItem = {
+			name: $('.name', $view).val(),
+			category: $('.category', $view).val()
+		};
 
-  	// Validation: both fields neccessary
-  	if (newListItem.name === '' || newListItem.category === '') {
-  		$('.error').show().text(invalidText);
-  		return;
-  	}
-  	else {
-  		$('.error').hide();
-  	}
+		// Validation: both fields neccessary
+		if (newListItem.name === '' || newListItem.category === '') {
+			$('.error').show().text(invalidText);
+			return;
+		}
+		else {
+			$('.error').hide();
+		}
     
-    // Creates the new item using the model
-  	listItems.create(newListItem);
+		// Creates the new item using the model
+		listItems.create(newListItem);
 
-  	// Clear out new items input field
-  	$('input[type="text"]', $view).val('');
+		// Clear out new items input field
+		$('input[type="text"]', $view).val('');
   
   });
 
