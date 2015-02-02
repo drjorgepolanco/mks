7a79e9ddc74bda4508edbd53d65e1ffee48b967e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-02 12:20:19 -0500

    Node.js Chat Server. Initial commit

---
+++ immersive/javascript/node/nodejs-chat-server/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = 3005;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.get('/', function (req, res) {
+  res.render('page');
+});
+app.use(express.static(__dirname + '/public'));
+
+var app = app.listen(port);
---
+++ immersive/javascript/node/nodejs-chat-server/public/chat.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  var messages = [];
+  var socket = io.connect('http://localhost:3005');
+  var field = document.getElementById('field');
+  var sendButton = document.getElementById('send');
+  var content = document.getElementById('content');
+
+  sendButton.onclick = function () {
+    var text = field.value;
+    socket.emit('send', { message: text });
+  };
+}
