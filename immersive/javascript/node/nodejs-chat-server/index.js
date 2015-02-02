9403b900c045950a8871f1f52281cc61b70f887a
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-02 12:37:15 -0500

    Node Chat working as expected

--- immersive/javascript/node/nodejs-chat-server/index.js
+++ immersive/javascript/node/nodejs-chat-server/index.js
@@ -1,13 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var port = 3005;
 
 app.set('views', __dirname + '/tpl');
-app.set('view engine', 'jade');
+app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
-app.get('/', function (req, res) {
-  res.render('page');
+app.get("/", function(req, res){
+  res.render("page");
 });
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
-var app = app.listen(port);
+// var app = app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { message: 'welcome to the chat' });
+  socket.on('send', function (data) {
+    io.sockets.emit('message', data);
+  });
+});
